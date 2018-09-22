1e18d83da37087b40520d8a4f78d0dd430bbebb1
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-09-22 16:39:13 +0300

    Lesson_4

---
+++ Lesson_4/Task2/Task2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double operand1 = 5, operand2 = 3;
+            double result = 0;
+
+            string sign = "";
+            while (sign!="exit") { 
+
+
+            Console.WriteLine("Please write down sign");
+
+            sign = Console.ReadLine();
+
+            switch (sign) {
+                case "-":
+                    {
+                        result = operand1 - operand2;
+                        break;
+                    }
+                case "+":
+                    {
+                        result = operand1 + operand2;
+                        break;
+
+                    }
+                case "*":
+                    {
+                        result = operand1 * operand2;
+                        break;
+
+                    }
+                case "/":
+                    {
+                        if (operand2 != 0)
+                        {
+                            result = operand1 / operand2;
+                        }
+                        else {
+                            Console.WriteLine("Unallowed value");
+
+                        }
+                        break;
+
+                    }
+                default:
+                    {
+
+                        Console.WriteLine("There is no such sign. Try again");
+                        break;
+
+                    }
+
+                    
+
+            }
+            Console.WriteLine("Result = {0}", result);
+           
+
+        }
+      }
+    }
+}
---
+++ Lesson_4/Task3/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Используя Visual Studio, создайте проект по шаблону Console Application.  
+                Напишите программу определения, попадает ли указанное пользователем число от 0 до 100
+                в числовой промежуток [0 - 14] [15 - 35] [36 - 50][50 - 100].
+                Если да, то укажите, в какой именно промежуток.
+                Если пользователь указывает число, не входящее ни в один из имеющихся числовых промежутков,
+                то выводится соответствующее сообщение. 
+            */
+
+            int num;
+            
+          //  string result;
+
+            Console.WriteLine("Enter any number from 0 to 100 to check interval");
+
+          //  if (num=Convert.ToInt16(Console.ReadLine()) {
+
+            }
+          
+
+            Console.WriteLine(num);
+            Console.ReadKey();
+                        
+           
+
+
+
+
+        }
+    }
+}
