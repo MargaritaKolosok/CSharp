85574b7dbb6b95aa6cc67c0ce7d4ed8dc3c22858
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-09-25 09:26:36 +0300

    Lesson_7 Task_3
    Corrections

--- Lesson_7/Lesson_7/Task_3/Program.cs
+++ Lesson_7/Lesson_7/Task_3/Program.cs
@@ -4,84 +4,80 @@ namespace Task_3
 {
     class Program
     {
-        static void Main(string[] args)
+
+        /*
+                 * 
+                 * Используя Visual Studio, создайте проект по шаблону Console Application.  
+    Напишите метод, который будет определять:  
+    1) является ли введенное число положительным или отрицательным.  
+    2) Является ли оно простым (используйте технику перебора значений).  
+
+
+    (Простое число – это натуральное число, которое делится на 1 и само на себя. Чтобы определить простое число или нет, следует найти все его целые делители. Если делителей больше 2-х, значит оно не простое.) 
+    3) Делится ли на 2, 5, 3, 6, 9 без остатка.
+
+
+        */
+
+        static string IsSimple(int val)
         {
-            /*
-                      * 
-                      * Используя Visual Studio, создайте проект по шаблону Console Application.  
-         Напишите метод, который будет определять:  
-         1) является ли введенное число положительным или отрицательным.  
-         2) Является ли оно простым (используйте технику перебора значений).  
+            string result;
 
+            int divisionsNumber = 0;
 
-         (Простое число – это натуральное число, которое делится на 1 и само на себя. Чтобы определить простое число или нет, следует найти все его целые делители. Если делителей больше 2-х, значит оно не простое.) 
-         3) Делится ли на 2, 5, 3, 6, 9 без остатка.
-
-
-                      */
-
-            int num;
-            string result = "";
-
-            Console.WriteLine("Enter number for analys");
-            num = Convert.ToInt32(Console.ReadLine());
-
-            string IsSimple(int val)
+            for (int i = 1; i <= Math.Abs(val); i++)
             {
-
-                int divisionsNumber = 0;
-
-                for (int i = 1; i == val; i++)
+                if ((Math.Abs(val) % i) == 0)
                 {
 
-
-                    Console.WriteLine(val % i);
-                    if ((val % i) == 0)
-                    {
-
-                        divisionsNumber++;
-                    }
+                    divisionsNumber++;
                 }
-
-                if (divisionsNumber == 2)
-                {
-
-                    result += " Simple" + divisionsNumber;
-
-                }
-                else
-                {
-                    result += " Not Simple" + divisionsNumber;
-                }
-
-                return result;
-
             }
 
-
-            string NumberAnalysis(int val)
+            if (divisionsNumber == 2)
             {
 
-                if (num > 0)
-                {
+               result = "Simple";
 
-                    result += "Positive ";
-
-                }
-                else
+            } else
                 {
-                    result += "Negative";
+                    result = "Not Simple";
                 }
 
-                return result;
-
-            }
-
-            Console.WriteLine(NumberAnalysis(num));
-              Console.WriteLine( IsSimple(num)); 
-            Console.ReadKey();
-
+            return result;
 
         }
+
+
+        static string NumberAnalysis(int val)
+        {
+            string result;
+
+            if (val > 0)
+            {
+
+                result = "Positive ";
+
+            } else
+                {
+                    result = "Negative";
+                }
+
+                return result;
+
+        }
+
+        static void Main(string[] args)
+        {
+       
+            Console.WriteLine("Enter number for analysis:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(NumberAnalysis(num));
+            Console.WriteLine(IsSimple(num));
+            Console.ReadKey();
+
+        }
+
     }
 }
