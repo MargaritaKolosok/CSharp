83c622423080be0a4c6a3054f1181ce3d03dbe12
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-09-28 20:50:29 +0300

    Lesson_9

--- Lesson_9/Task_1-1/Task_1-1/Program.cs
+++ Lesson_9/Task_1-1/Task_1-1/Program.cs
@@ -5,29 +5,46 @@ namespace Task_1_1
     
     class Program
     {
-        static int[] ModifyArray(int[] array, int modifier)
+        static int[] ModifyArray(int[] array, int arrLength)
         {
             for (int i=0; i<array.Length; i++)
             {
-                array[i] = modifier * i;
+                array[i] = arrLength * i;
             }
 
             return array;
         }
 
+        static int ArrayAnalysis(int[] array, out int max)
+        {
+            max = 0;
+            for (int i=0; i<array.Length; i++)
+            {
+                if (array[i]>max)
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+
         static void Main(string[] args)
         {
-            
-                       
+                                   
             Console.WriteLine("Enter number of array elements:");
-            int modifier = Convert.ToInt32(Console.ReadLine());
-            int[] myArray = new int [modifier];
-            myArray = ModifyArray(myArray, modifier);
+            int arrLength = Convert.ToInt32(Console.ReadLine());
+
+            int[] myArray = new int [arrLength];
+
+            myArray = ModifyArray(myArray, arrLength);
+            int max = ArrayAnalysis(myArray, out int max1);
+
             for (int i=0; i<myArray.Length; i++)
             {
                 Console.WriteLine("Array element {0}", myArray[i]);
             }
-            
+
+            Console.WriteLine("Array MAX element {0}", max);
             Console.ReadKey();
         }
     }
