703ac692f23dc2d01e93a582bdd650338a030f6c
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-10-02 21:58:22 +0300

    MSDN

---
+++ Lesson_9/MSDN_Examples/Example_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Example_1
+{    class Program
+    {
+        public static void PrintValues(int[] myArr)
+        {
+            foreach (int i in myArr)
+            {
+                Console.Write("\t{0}", i);
+            }
+            Console.WriteLine();
+        }
+        public static void PrintValues(Object[] myArr)
+        {
+            foreach (Object i in myArr)
+            {
+                Console.Write("\t{0}", i);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            
+            int[] InitialArray = new int[5] { 1,2,3,4,5};
+            Object[] ObjectArray = new Object[5] { 6,7,8,9,10};
+
+            PrintValues(InitialArray);
+            PrintValues(ObjectArray);
+            System.Array.Copy(InitialArray, ObjectArray, 3);
+            PrintValues(InitialArray);
+            PrintValues(ObjectArray);
+            Console.ReadKey();
+
+        }
+    }
+}
