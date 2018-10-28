b65b3229f0c37a9f2d573ba8da6da47704c848f0
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-10-28 21:39:20 +0200

    Task_3

--- EssentialLessons/Lesson_1/Lesson_1/Task_3/Program.cs
+++ EssentialLessons/Lesson_1/Lesson_1/Task_3/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace Task_3
 {
+    class myClass
+    {
+        private string field = null;
+
+        public string Field
+        {
+            set
+            {
+                field = value;
+            }
+
+            get
+            {
+                return field;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            myClass instance = new myClass();
+
+            Console.WriteLine(instance.Field);
+            instance.Field = "Hello";
+            Console.WriteLine(instance.Field);
+
+
+            Console.ReadKey();
         }
     }
 }
