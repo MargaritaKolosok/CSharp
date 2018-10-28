1f5e1fcc8f726a44db5c79c097a8c36835dfe836
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-10-28 21:33:20 +0200

    Task_2

---
+++ EssentialLessons/Lesson_1/Lesson_1/Task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class myClass
+    {
+        private string field = null;
+
+        public void SetString(string value)
+        {
+            field = value;
+        }
+
+        public string GetString()
+        {
+            return field;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass instance = new myClass();
+            Console.WriteLine(instance.GetString());
+
+            instance.SetString("Hello");
+            
+            Console.WriteLine(instance.GetString());
+            Console.ReadKey();
+        }
+    }
+}
