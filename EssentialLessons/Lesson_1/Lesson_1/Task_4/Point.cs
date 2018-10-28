a09e2258615e965714207d960062aa06633c9dff
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-10-28 21:56:43 +0200

    Lesson_4

---
+++ EssentialLessons/Lesson_1/Lesson_1/Task_4/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Point
+    {
+        private int x, y;
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+
+        public Point()
+        {
+            Console.WriteLine("Default constructor!");
+        }
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ EssentialLessons/Lesson_1/Lesson_1/Task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point PointA = new Point(122,44);
+            Console.WriteLine("Point.X = {0}, Point.Y = {1}", PointA.X, PointA.Y);
+
+            Point PointB = new Point();
+            Console.WriteLine("Point.X = {0}, Point.Y = {1}", PointB.X, PointB.Y);
+            Console.ReadKey();
+
+        }
+    }
+}
