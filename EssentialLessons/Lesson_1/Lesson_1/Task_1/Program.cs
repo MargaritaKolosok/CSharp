c23ec8ce3a035785a8b2dbd979714cb18a04c1d4
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-10-28 21:19:48 +0200

    Lesson_1

---
+++ EssentialLessons/Lesson_1/Lesson_1/Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class myClass
+    {
+        public string field;
+        public void Method()
+        {
+            Console.WriteLine(field);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass instance = new myClass();
+            instance.field = "Hello World!";
+
+            instance.Method();
+
+            myClass instance2 = new myClass();
+            instance2.field = "Was nice to meet you!";
+            instance2.Method();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
