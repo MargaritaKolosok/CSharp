16ede3846b78097dbb349f7e22d90884cee489c2
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-01 15:38:25 +0200

    Task_2

---
+++ EssentialLessons/Lesson_2/Task_1/Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class Building
+    {
+        public int Area;
+        public int Person;
+
+        public void CountArea()
+        {       
+           Console.WriteLine("Area per person {0}", Area / Person);
+         
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building house = new Building();
+            house.Area = 500;
+            house.Person = 20;
+            house.CountArea();
+
+            Console.ReadKey();
+           
+        }
+    }
+}
