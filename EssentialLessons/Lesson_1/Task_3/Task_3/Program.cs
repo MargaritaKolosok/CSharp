e824295153ef52650dcb2ee284e69411fe40df68
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-07 22:28:08 +0200

    Task_3

---
+++ EssentialLessons/Lesson_1/Task_3/Task_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CheckNumber
+{
+    public bool IsSimple(int x)
+    {
+        if (x <= 1)  return false; 
+
+        for (int i = 2; i <= x/i; i++)
+        
+            if ((x % i) == 0)  return false; 
+
+           return true; 
+
+        
+    }
+}
+
+namespace Task_3
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            CheckNumber myNumber = new CheckNumber();
+
+            for (int i=2; i<10; i++)
+            {
+                if (myNumber.IsSimple(i))
+                {
+                    Console.WriteLine("{0} is Simple Number", i);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is NOT Simple Number", i);
+                }
+               
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
