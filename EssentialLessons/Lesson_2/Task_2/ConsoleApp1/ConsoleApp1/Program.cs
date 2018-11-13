4eafae2ca2cba89775c9329e2e23b79e6c7079a4
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-13 21:39:50 +0200

    Task_3

--- EssentialLessons/Lesson_2/Task_2/ConsoleApp1/ConsoleApp1/Program.cs
+++ EssentialLessons/Lesson_2/Task_2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,13 @@ class Building
         public int Occupants;
         public int Area;
 
+    public Building(int f, int o, int a)
+        {
+        Floors = f;
+        Occupants = o;
+        Area = a;
+        }
+
     public int AreaPerPerson()
         {
         return Area / Occupants;
@@ -27,15 +34,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Building Office = new Building();
-            Building House = new Building();
+            Building Office = new Building(3,5,100);
+           
 
             int minArea = 5;
 
-            Office.Area = 100;
-            Office.Occupants = 12;
+        //    Office.Area = 100;
+        //    Office.Occupants = 12;
             Console.WriteLine("Max person number possible to be in office is {0}", Office.MinArea(minArea));
             Console.WriteLine("Min Area Per person is {0}", Office.AreaPerPerson());
+
+            Building House = new Building(4, 12, 250);
+            Console.WriteLine("Max person number possible to be in office is {0}, Ocupants = {1}, Area = {2}", House.MinArea(minArea), House.Occupants, House.Area);
             Console.ReadKey();
         }
     }
