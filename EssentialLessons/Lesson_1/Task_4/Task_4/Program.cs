1f18f62c3e2789c5b897b8fc800ed1bebf01bcfb
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-15 09:11:45 +0200

    Array

---
+++ EssentialLessons/Lesson_1/Task_4/Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Building
+    {
+        public Building 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ EssentialLessons/Lesson_1/Task_5/Task_5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                double sqrtNum;
+                sqrtNum = Math.Pow(i,2);
+                Console.WriteLine("POW of {0} is {1}", i, sqrtNum);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
