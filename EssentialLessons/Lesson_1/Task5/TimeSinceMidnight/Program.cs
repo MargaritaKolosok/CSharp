9df522adc3c9f3d5b17772a909dfe3a618c005f6
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-16 16:51:20 +0200

    Time

---
+++ EssentialLessons/Lesson_1/Task5/TimeSinceMidnight/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSinceMidnight
+{
+    public class CountMinutes
+   {
+        public int minutes;
+        public CountMinutes(int h, int m)
+        {
+            this.minutes = 60 * h + m;
+        }
+   }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
