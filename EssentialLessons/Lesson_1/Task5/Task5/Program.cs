f41da59dd51b2fabf4eeb12f9b7a9c18f779f751
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-16 16:42:09 +0200

    Class + this

---
+++ EssentialLessons/Lesson_1/Task5/Task5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Rect
+    {
+        public int Height;
+        public int Width;
+
+        public Rect(int Width, int Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public int Area()
+        {
+            return this.Width * this.Height;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rect myRect = new Rect(3,5);
+            Console.WriteLine("Width is {0}, Height is {1}, Area is {2}", myRect.Width, myRect.Height, myRect.Area());
+            Console.ReadKey();
+        }
+    }
+}
