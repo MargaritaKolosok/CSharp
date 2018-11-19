c354c1879ff57ad9729bd6e78528158740a6ba5b
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-19 20:14:29 +0200

    get, set
    class Adress
    {
        public int Index { set; get; }
        public string Country { set; get; }
    }

--- EssentialLessons/Lesson_1/LessonFromCourse1/LessonFromCourcev2/Program.cs
+++ EssentialLessons/Lesson_1/LessonFromCourse1/LessonFromCourcev2/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+class Adress
+{
+    public int Index { set; get; }
+    public string Country { set; get; }
+}
+
 namespace LessonFromCourcev2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Adress myHome = new Adress();
+            myHome.Country = "Ukraine";
+            myHome.Index = 12345;
+
+            Console.WriteLine("My home adress is {0} {1}", myHome.Index, myHome.Country);
+            Console.ReadKey();
+
         }
     }
 }
