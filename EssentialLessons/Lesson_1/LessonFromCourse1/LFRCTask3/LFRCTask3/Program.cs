d9942931e020021e0e4a83eaff189ef7eeedbf7f
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-20 12:20:24 +0200

    Console color added

--- EssentialLessons/Lesson_1/LessonFromCourse1/LFRCTask3/LFRCTask3/Program.cs
+++ EssentialLessons/Lesson_1/LessonFromCourse1/LFRCTask3/LFRCTask3/Program.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 /*
-Создать класс Book. Создать классы Title, Author и Content, каждый из которых должен содержать
+Создать класс Book.
+Создать классы Title, Author и Content, каждый из которых должен содержать
 одно строковое поле и метод void Show().
+
 Реализуйте возможность добавления в книгу названия книги, имени автора и содержания.
 Выведите на экран разными цветами при помощи метода Show() название книги, имя автора и
 содержание. 
+
 */
 
 class Book
@@ -29,13 +33,14 @@ class Book
 
 class Author
 {
-    public string BookAuthor;
+    private string BookAuthor;
     public string SetAuthorName
 {
         set { BookAuthor = value; }
 }
     public void Show()
     {
+        Console.BackgroundColor = ConsoleColor.Red;
         Console.WriteLine(BookAuthor);
     }
     
