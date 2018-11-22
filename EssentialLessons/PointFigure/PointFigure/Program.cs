7f5eb7b09f1c43998df4ecd166f874ccf7d4474b
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-22 10:22:32 +0200

    Update Program.cs
    added universal setValue

--- EssentialLessons/PointFigure/PointFigure/Program.cs
+++ EssentialLessons/PointFigure/PointFigure/Program.cs
@@ -36,9 +36,10 @@ class Point
     }
     public string FigureName
     {
-        get {
+        get
+        {
             return figureName;
-            }
+        }
     }
     public Point(int x, int y)
     {
@@ -51,7 +52,18 @@ class Figure
 {
     private Point point1, point2, point3, point4, point5;
     private double sideLendth;
-    
+    private int sideNumber;
+
+    public Figure(int sideNumber)
+    {
+        this.sideNumber = sideNumber;
+    }
+    public int SideNumber
+    {
+        get { return sideNumber;  }
+    }
+
+    /*
     public Figure(Point point1, Point point2, Point point3)
     {
         this.point1 = point1;
@@ -73,6 +85,23 @@ class Figure
         this.point3 = point4;
         this.point3 = point5;
     }
+    */
+    public void setPointValues()
+    {
+        Point[] pointArray = new Point[sideNumber];
+
+        for (int i=0; i<sideNumber; i++)
+        {
+            int x, y;
+            Console.WriteLine("Enter X for Point {0}", i);
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Y for Point {0}", i);
+            y = Convert.ToInt32(Console.ReadLine());
+
+            pointArray[i] = new Point(x, y);
+        }
+    }
     public double LengthSide(Point A, Point B)
     {
         sideLendth = Math.Sqrt(Math.Pow((point2.X - point1.X), 2) + Math.Pow((point2.Y - point1.Y), 2));
@@ -80,7 +109,7 @@ class Figure
     }
     public void PerimeterCalculator()
     {
-                    
+
     }
 }
 namespace PointFigure
@@ -89,19 +118,27 @@ namespace PointFigure
     {
         static void Main(string[] args)
         {
-            Point point1 = new Point(1,2);
+            /*
+            Point point1 = new Point(1, 2);
             Point point2 = new Point(4, 6);
             Point point3 = new Point(2, 5);
             Point point4 = new Point(6, 17);
             Point point5 = new Point(7, 9);
 
-            Console.WriteLine("X is {0}, y is {1}", point1.X, point1.Y);
+    */
 
-            Figure Triangle = new Figure(point1, point2, point3);
-            
-            Console.WriteLine(Triangle.LengthSide(point1, point2)); 
+            int sideNumber;
+            Console.Write("Enter number of sides in the Figure:");
+            sideNumber = Convert.ToInt32(Console.ReadLine());
+
+            Figure Triangle = new Figure(sideNumber);
+            Triangle.setPointValues();
+
+            Console.WriteLine(Triangle.SideNumber);
+
+           // Console.WriteLine(Triangle.LengthSide(point1, point2));
             Console.ReadKey();
-            
+
         }
     }
 }
