1bb9c6db518924533688eb9fbb1eb13b5514c3f9
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-22 21:25:12 +0200

    a
    a

---
+++ EssentialLessons/Lesson_1/TFLTask2/TFLTask2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Создать класс Book. Создать классы Title, Author и Content, каждый из которых должен содержать
+одно строковое поле и метод void Show().
+Реализуйте возможность добавления в книгу названия книги, имени автора и содержания.
+Выведите на экран разными цветами при помощи метода Show() название книги, имя автора и
+содержание. 
+     */
+class Book
+{
+    public Book(string BookTitle, string BookAutor, string BookContent)
+    {
+       
+    }
+    class Title
+    {
+        public string BookTitle;
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(BookTitle);
+        }
+    }
+    class Autor
+    {
+        public string BookAutor;
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(BookAutor);
+        }
+    }
+
+    class Content
+    {
+        public string BookContent;
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(BookContent);
+        }
+    }
+}
+
+namespace TFLTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
