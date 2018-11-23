0f49d0f40bc3d7fff52a2c90f4a410cee73116ff
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-23 10:24:02 +0200

    Sallary count
    Sallary count

---
+++ EssentialLessons/Employee/Employee/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ Создать класс Employee.
+В теле класса создать пользовательский конструктор, который принимает два строковых аргумента, и
+инициализирует поля, соответствующие фамилии и имени сотрудника.
+Создать метод рассчитывающий оклад сотрудника (в зависимости от должности и стажа) и налоговый
+сбор.
+Написать программу, которая выводит на экран информацию о сотруднике (фамилия, имя, должность),
+оклад и налоговый сбор. 
+ * */
+class Employee
+{
+    private string name, surname;
+    public double Tax, Sallary;
+
+    public Tax()
+        {
+         get {return  tax; }
+
+        }
+
+
+    public Employee(string name, string surname)
+    {
+        this.name = name;
+        this.surname = surname;
+    }
+    public double CountSallary(string position, int experience)
+    {
+        double sallary;
+        double baseSallary = 28000;
+        if (position == "QA")
+        {
+            sallary = (experience + 0.1) * 0.2 * baseSallary;
+
+        }
+        else if (position == "MANAGER")
+        {
+            sallary = (experience + 0.1) * 0.37 * baseSallary;
+        }
+        else if (position == "PROGRAMMER")
+        {
+            sallary = (experience + 0.1) * 0.46 * baseSallary;
+        }
+        else
+        {
+            sallary = (experience + 0.1) * 0.11 * baseSallary;
+        }
+        // 
+        return sallary;
+
+    }
+    public double CountTax(double x)
+    {
+        return (x / 100) * 5.5;
+    }
+
+
+    public void getInfo()
+    {
+        Console.WriteLine("Employee {0} {1} has sallary {2}. Tax from sallary is {3}", name, surname);
+    }
+            
+}
+
+namespace EmployeeSallary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee John = new Employee("John", "Smith");
+            double emSallary = John.CountSallary("QA", 4);
+            double emTax = John.CountTax(emSallary);
+
+
+            Console.ReadKey();
+
+        
+            
+
+        }
+    }
+}
