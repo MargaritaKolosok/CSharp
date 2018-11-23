f81ce28570fa59ca36ab08c80c9292f035134b0b
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-23 11:39:55 +0200

    SallarySize & TaxSize

--- EssentialLessons/Employee2/Employee2/Program.cs
+++ EssentialLessons/Employee2/Employee2/Program.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 class Sallary
 {
-    int baseSallary = 32000;
-    double sallary=1, tax=1, experience;
+    public int baseSallary = 32000;
+    double sallary, tax, experience;
 
     public Sallary(double experience)
     {
@@ -16,15 +16,21 @@ class Sallary
 
     public double SallarySize
     {
-        set { sallary = baseSallary * 0.2 * experience; }
-        get { return sallary; }
+    get {
+            sallary = baseSallary * 0.2 * experience;
+            return sallary;
+        }
                
     }
     public double TaxSize
     {
-        set { tax = (sallary /100)*25; }
-        get { return tax; }
+       get
+        {
+            tax = (sallary / 100) * 25;
+            return tax;
+        }
     }
+  
 
 }
 
@@ -42,11 +48,14 @@ class Employee
         Console.WriteLine("Hello {0}, please enter year of experience and Position enter ", name);
         Console.Write("Experience years:");
         double experience = Convert.ToInt32(Console.ReadLine());
+
        // Console.Write("Position:");
        // string position = Convert.ToString(Console.ReadLine());
 
         Sallary sallary = new Sallary(experience);
-        Console.WriteLine("Sallary: {0}, Tax: {1}", sallary.SallarySize, sallary.SallarySize);
+       
+
+        Console.WriteLine("Sallary: {0}, Tax: {1}", sallary.SallarySize, sallary.TaxSize);
     }
     
     
