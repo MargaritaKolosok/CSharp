e27ef19a1cfb0b48a73ad62cdaeb8bc50617eac2
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-29 09:19:36 +0200

    AsIs

---
+++ EssentialLessons/LessonsFromMSDN/OperatorAsIs/OperatorAsIs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Person
+{
+    string Name;
+}
+class Child : Person
+{
+    int Age;
+}
+namespace OperatorAsIs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "My test string";
+            Console.WriteLine("Test is string {0}", test is string);
+            Child Mikky = new Child();
+            Console.WriteLine("Child is Person {0}", Mikky is Person);
+            Console.ReadKey();
+        }
+    }
+}
