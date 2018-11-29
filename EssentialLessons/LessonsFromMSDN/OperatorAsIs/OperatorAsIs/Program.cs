0ccb462e674ec494dcd3bbc1f27835b884325e1e
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-29 09:31:33 +0200

    Update Program.cs

--- EssentialLessons/LessonsFromMSDN/OperatorAsIs/OperatorAsIs/Program.cs
+++ EssentialLessons/LessonsFromMSDN/OperatorAsIs/OperatorAsIs/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 class Person
 {
-    string Name;
+   public string Name;
 }
 class Child : Person
 {
@@ -23,10 +23,17 @@ namespace OperatorAsIs
         {
             string test = "My test string";
             Console.WriteLine("Test is string {0}", test is string);
+
+            Person Frank = new Person();
+            Frank.Name = "Frank Jon";
+            Console.WriteLine("Frank is Person: {0}", Frank is Person);
+
             Child Mikky = new Child();
             Console.WriteLine("Child is Person {0}", Mikky is Person);
+
             Flower rose = new Flower();
             Console.WriteLine("Is Flower a Person {0}", rose is Person);
+
             Console.ReadKey();
         }
     }
