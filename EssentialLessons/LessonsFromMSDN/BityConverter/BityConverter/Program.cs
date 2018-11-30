e966a71e4ce9e4b11fe84c30ae96f1709842beb8
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-30 14:22:19 +0200

    Bite in KB

---
+++ EssentialLessons/LessonsFromMSDN/BityConverter/BityConverter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+килобайт	КБайт (KБ)	1024 байта
+мегабит	мбит (мб)	1 000 килобит
+мегабайт	МБайт (МБ)	1024 килобайта
+гигабит	гбит (гб)	1 000 мегабит
+гигабайт	ГБайт (ГБ)	1024 мегабайта
+терабит	тбит (тб)	1 000 гигабит
+терабайт	ТБайт (ТБ)	1024 гигабайта
+     */
+class Bite
+{
+    private long bite;
+    public long BiteNum
+    {
+        set => bite = value;
+        get => bite;
+    }
+    public Bite(long bite)
+    {
+        BiteNum = bite;
+    }
+    public void ConvertToKB()
+    {
+        Console.WriteLine("B {0} in KB {1}", BiteNum, BiteNum / 1024);
+    }
+
+   
+   
+}
+
+namespace BityConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bite muBite = new Bite(1024);
+            muBite.ConvertToKB();
+            Console.ReadLine();
+        }
+    }
+}
