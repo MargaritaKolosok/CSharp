65deb7c30fe113e6f02a57b1b28fa8fc0952b043
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-30 20:37:55 +0200

    Update Program.cs

--- EssentialLessons/LessonsFromMSDN/stringObject/stringObject/Program.cs
+++ EssentialLessons/LessonsFromMSDN/stringObject/stringObject/Program.cs
@@ -17,23 +17,7 @@ namespace stringObject
             int result2 = string.Compare(str1, str3);
             Console.WriteLine("String 1 = '{0}, String 2 = '{1}', String 3 = '{2}", str1, str2, str3);
             Console.WriteLine("Compare result for STR1 and STR2 {0}, Compare result for STR1 and STR3 {1}", result1, result2);
-
-
-            string[] digits = { "нуль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };            
-            int num = Convert.ToInt32(Console.ReadLine());
-            int arrayLength = num.ToString().Length;
-            string[] newArray = new string[arrayLength];
-            string stringNum = num.ToString();
-
-            for (int i=0; i<arrayLength; i++)
-            {
-                newArray[i] = digits[Convert.ToInt32(stringNum[i])];
-                Console.WriteLine(newArray[i] + " ");
-
-            }
-            
-
-
+                       
             Console.ReadKey();
         }
     }
