0d4366d7883e9a31f46f970ba7871544f5651463
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-11-30 16:11:53 +0200

    string compare

---
+++ EssentialLessons/LessonsFromMSDN/stringObject/stringObject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "string 1";
+            string str2 = "string 1";
+            string str3 = "string 3";
+            int result1 = string.Compare(str1, str2);
+            int result2 = string.Compare(str1, str3);
+            Console.WriteLine("String 1 = '{0}, String 2 = '{1}', String 3 = '{2}", str1, str2, str3);
+            Console.WriteLine("Compare result for STR1 and STR2 {0}, Compare result for STR1 and STR3 {1}", result1, result2);
+            Console.ReadKey();
+        }
+    }
+}
