c72c73aaf91ae6781055c2831ff7f6f4903cf550
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-01 10:33:51 +0200

    Number converter

---
+++ EssentialLessons/LessonsFromMSDN/stringObject/NumberConverter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumberConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] digits = { "нуль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять" };
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int stringLength = (num.ToString()).Length;
+
+            string newString = "";
+            string stringNum = num.ToString();
+          //  int[] myNum = int.Parse(stringNum);
+
+            for(int i=0; i<stringLength; i++)
+            {
+              newString += stringNum.ChartAt(i);
+            }
+
+            Console.WriteLine(newString);
+            Console.ReadKey();
+        }
+    }
+}
