209208860724acd82d04b41b5f414e1c4fabb337
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-01 22:29:11 +0200

    Interface

---
+++ EssentialLessons/LessonsFromMSDN/InterfaceNum/InterfaceNum/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public interface ISeria
+{
+    int GetNext();
+    void Reset();
+    void setStart(int x);
+}
+class TwoD : ISeria
+{
+    int start, num;
+    public TwoD()
+    {
+        start = 0;
+        num = 0;
+    }
+    public int GetNext()
+    {
+        num += 2;
+        return num;
+    }
+    public void setStart(int x)
+    {
+        num = x;
+    }
+    public void Reset()
+    {
+        num = start;
+    }
+}
+
+namespace InterfaceNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoD myNum = new TwoD();
+            for (int i=0; i<10; i++)
+            {
+                Console.WriteLine(myNum.GetNext());
+            }
+
+            myNum.setStart(100);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(myNum.GetNext());
+            }
+            myNum.Reset();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(myNum.GetNext());
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
