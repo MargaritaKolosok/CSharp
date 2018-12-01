441f4587cf2f4f5258a882095f8cd5efb8ac0b84
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-01 17:25:59 +0200

    redactor Task 2

---
+++ EssentialLessons/Lesson_4/Task_2/Task_2/AbstractHandler.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    abstract class AbstractHandler
+    {
+        protected string fileName;
+
+        public AbstractHandler(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public abstract void Open();
+        public abstract void Create();
+        public abstract void Change();
+        public abstract void Save();
+    }
+    class XMLHandler : AbstractHandler
+    {
+        public XMLHandler(string fileName)
+            : base(fileName)
+        {
+
+        }
+        public override void Open()
+        {
+            Console.WriteLine("XML Opened");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("XML Created");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("XML Changed");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("XML Saved");
+        }
+    }
+
+    class TXTHandler : AbstractHandler
+    {
+        public TXTHandler(string fileName)
+            : base(fileName)
+        {
+
+        }
+        public override void Open()
+        {
+            Console.WriteLine("TXT Opened");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("TXT Created");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("TXT Changed");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("TXT Saved");
+        }
+    }
+
+    class DOCHandler : AbstractHandler
+    {
+        public DOCHandler(string fileName)
+            : base(fileName)
+        {
+        }
+        public override void Open()
+        {
+            Console.WriteLine("DOC Opened");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("DOC Created");
+        }
+        public override void Change()
+        {
+            Console.WriteLine("XDOC Changed");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("DOC Saved");
+        }
+    }
+}
---
+++ EssentialLessons/Lesson_4/Task_2/Task_2/Redactor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    class Redactor
+    {
+        AbstractHandler doc;
+
+        public void ChooseFile(string file)
+        {           
+            int index = file.IndexOf('.');
+            string  formatFile = file.Substring(index);
+            string fileName = file;
+
+            switch (formatFile)
+            {
+                case ".txt":
+                    {
+                        doc = new TXTHandler(fileName);
+                        break;
+                    }
+                case ".xml":
+                    {
+                        doc = new XMLHandler(fileName);
+                        break;
+                    }
+                case ".doc":
+                    {
+                        doc = new DOCHandler(fileName);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Unknown format");
+                        break;
+                    }
+            }
+
+        }
+        public void Open()
+        {
+            doc.Open();
+        }
+        public void Change()
+        {
+            doc.Change();
+        }
+        public void Save()
+        {
+            doc.Save();
+        }
+        public void Create()
+        {
+            doc.Create();
+        }
+    }
+}
