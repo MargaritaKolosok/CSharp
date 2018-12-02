5260c7105e87126d36d9cf7265383d6260961e73
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-02 10:00:54 +0200

    Interface

---
+++ EssentialLessons/LessonsFromMSDN/isEvenInterface/isEvenInterface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+interface IEven
+{
+    bool IsEven(int x);
+    bool IsOdd(int x);
+}
+
+class checkEven : IEven
+{
+    bool IEven.IsEven(int x)
+    {
+        if (x % 2 != 0)
+            return true;
+        return false;
+    }
+    public bool IsOdd(int x)
+    {
+        IEven ob = this;
+        return !ob.IsEven(x);
+    }
+}
+namespace isEvenInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            checkEven myNum = new checkEven();
+            bool result;
+            Console.WriteLine("result = myNum.IsOdd(4);");
+            result = myNum.IsOdd(4);
+            Console.WriteLine(result);
+            Console.WriteLine("IEven ob = (IEven)myNum;");
+            IEven ob = (IEven)myNum;
+            Console.WriteLine("ob.IsEven(4) : {0}, ob.IsOdd(5) {1}", ob.IsEven(4), ob.IsOdd(5));
+            Console.ReadKey();
+        }
+    }
+}
