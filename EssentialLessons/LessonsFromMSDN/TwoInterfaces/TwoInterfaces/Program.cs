f69891147c3ff4ea1563b38d851ed728fca1dd89
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-02 10:12:30 +0200

    TwoInterfaces

---
+++ EssentialLessons/LessonsFromMSDN/TwoInterfaces/TwoInterfaces/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+interface IMyIF_A
+{
+    int Meth(int x);
+}
+interface IMyIF_B
+{
+    int Meth(int x);
+}
+class myClass : IMyIF_A, IMyIF_B
+{
+    int IMyIF_A.Meth(int x)
+    {
+       return x+x;
+    }
+    int IMyIF_B.Meth(int x)
+    {
+        return x * x;
+    }
+    public int MethA(int x)
+    {
+        IMyIF_A ob = this;
+        return ob.Meth(x);
+    }
+    public int MethB(int x)
+    {
+        IMyIF_B ob = this;
+        return ob.Meth(x);
+    }
+
+}
+namespace TwoInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass myNum = new myClass();
+            
+            myNum.MethA(3);
+            myNum.MethB(3);
+            Console.ReadKey();
+        }
+    }
+}
