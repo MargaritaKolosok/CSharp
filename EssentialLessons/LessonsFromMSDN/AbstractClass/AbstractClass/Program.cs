2b4a8f83375511e9776950499afa602b5afc8b8c
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-03 10:37:46 +0200

    AbstractClass task

---
+++ EssentialLessons/LessonsFromMSDN/AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+abstract class baseClass
+{
+    protected int x;
+    protected int y;
+    public abstract void Method();
+    public abstract int X { get; set; }
+    public abstract int Y { get; set; }
+}
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
