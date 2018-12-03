b73060b9a4bb675f29426f3d5ef7d15fbd24de61
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-03 09:02:55 +0200

    Task_3

---
+++ EssentialLessons/Lesson_4/Task_3/Task_3/Task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Создайте 2 интерфейса IPlayable и IRecodable. В каждом из интерфейсов создайте по 3 метода void
+Play() / void Pause() / void Stop() и void Record() / void Pause() / void Stop() соответственно.
+Создайте производный класс Player от базовых интерфейсов IPlayable и IRecodable.
+Написать программу, которая выполняет проигрывание и запись. 
+ 
+     */
+
+interface IPlayable
+{
+    void Play();
+    void Pause();
+    void Stop();
+}
+
+interface IRecordable
+{
+    void Record();
+    void Pause();
+    void Stop();
+}
+class Player : IPlayable, IRecordable
+{
+
+}
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
