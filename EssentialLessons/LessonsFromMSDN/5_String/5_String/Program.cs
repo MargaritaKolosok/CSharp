b71de8d949212affe3e4ec9de52354a389def291
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-04 14:51:09 +0200

    string

---
+++ EssentialLessons/LessonsFromMSDN/5_String/5_String/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class MyString
+{
+    private string mystring;
+    public MyString(string mystring)
+    {
+        this.mystring = mystring;
+    }
+    public void Add(string newString)
+    {
+        mystring += " " + newString;
+    }
+    public string ShowString()
+    {
+        return mystring;
+    }
+    public string DeleteWord(string word)
+    {
+        if (mystring.IndexOf(word)!=-1)
+        {
+            mystring = mystring.Remove(mystring.IndexOf(word), word.Length);
+        }
+
+        return mystring;
+    }
+}
+namespace _5_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyString testString = new MyString("Hello");
+            testString.Add("my");
+            testString.Add("Friend!");
+            Console.WriteLine(testString.ShowString());
+            testString.DeleteWord("my");
+            Console.WriteLine(testString.ShowString());
+            Console.ReadKey();
+        }
+    }
+}
