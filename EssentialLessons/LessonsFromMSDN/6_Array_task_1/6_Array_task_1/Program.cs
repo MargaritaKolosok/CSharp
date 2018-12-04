092e1122d6d6cb6bcbbc4dff063a18b44870652f
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-04 15:14:41 +0200

    Array

---
+++ EssentialLessons/LessonsFromMSDN/6_Array_task_1/6_Array_task_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ArrayCreator
+{
+    private int[] arr;
+    public ArrayCreator(int numOfEl)
+    {
+        arr = new int[numOfEl];
+    }
+    public void AddRandomElements()
+    {
+        Random random = new Random();
+        for (int i=0; i<arr.Length; i++)
+        {
+            
+            arr[i] = random.Next(1,100);
+        }
+    }
+    private int Average()
+    {
+        int sum = 0;
+        for (int i=0; i<arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum / arr.Length;
+    }
+    public void Show()
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {           
+            Console.WriteLine(arr[i]);
+        }
+        Console.WriteLine("Average num in array: {0}", Average());
+    }
+}
+namespace _6_Array_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayCreator myArray = new ArrayCreator(6);
+            myArray.AddRandomElements();
+           
+            myArray.Show();
+            Console.ReadKey();
+        }
+    }
+}
