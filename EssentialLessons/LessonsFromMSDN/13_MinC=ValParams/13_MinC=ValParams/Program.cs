929600dc754a6ee756a69f66d4fd4694af4f3f3f
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-06 08:58:36 +0200

    MinParams

---
+++ EssentialLessons/LessonsFromMSDN/13_MinC=ValParams/13_MinC=ValParams/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MinVal
+{
+    int[] arr;
+    public MinVal(params int[] num)
+    {
+        arr = num;
+    }
+    public int Min()
+    {
+        if (arr.Length == 0)
+        {
+            Console.WriteLine("No elements in array");
+            return 0;
+        }
+        int min = arr[0];
+        for (int i=1; i<arr.Length; i++)
+        {
+            if (min>arr[i])
+            {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
+
+namespace _13_MinC_ValParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinVal min1 = new MinVal(1, 5, 6, 7, 8, 9, -2);
+            Console.WriteLine(min1.Min());
+            MinVal min2 = new MinVal();
+            Console.WriteLine(min2.Min());
+
+            Console.ReadKey();
+        }
+    }
+}
