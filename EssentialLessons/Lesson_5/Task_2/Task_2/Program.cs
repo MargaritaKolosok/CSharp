96d0a5ef0b71f6206fb726d87e9d692e385d96b7
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-07 15:33:19 +0200

    array task

---
+++ EssentialLessons/Lesson_5/Task_2/Task_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Требуется:
+Создать массив размерностью N элементов, заполнить его произвольными целыми значениями.
+Вывести наибольшее значение массива, наименьшее значение массива, общую сумму элементов,
+среднее арифметическое всех элементов, вывести все нечетные значения. 
+*/
+
+class myArr
+{
+    private int[] arr;
+    Random random = new Random();
+
+    public myArr(int x)
+    {
+      arr = new int[x];
+        
+        for (int i=0; i<arr.Length; i++)
+        {
+            arr[i] = (int)random.Next(1,10);
+        }     
+    }
+
+    int max, min, sum, average;
+    public void Max()
+    {
+        max = arr[0];
+        for(int i=0; i<arr.Length; i++)
+        {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        Console.WriteLine("Max value is {0}", max);
+    }
+    public void ShowElemets()
+    {
+        foreach (int x in arr)
+        {
+            Console.WriteLine("Elemets array index {0} = {1}", x, arr[x]);
+        }
+    }
+
+
+}
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myArr my = new myArr(10);
+            my.Max();
+            my.ShowElemets();
+            Console.ReadKey();
+        }
+    }
+}
