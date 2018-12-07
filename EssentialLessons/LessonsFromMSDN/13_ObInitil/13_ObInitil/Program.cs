3534590c6a96103bfc2d475c6c604818dce508f7
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-07 12:39:17 +0200

    Update Program.cs

--- EssentialLessons/LessonsFromMSDN/13_ObInitil/13_ObInitil/Program.cs
+++ EssentialLessons/LessonsFromMSDN/13_ObInitil/13_ObInitil/Program.cs
@@ -8,6 +8,22 @@ class Person
     public string Name;
     public int age;
 }
+class myClass
+  {
+    int x, y;
+    static public myClass Factory(int x, int y)
+        {
+            myClass t = new myClass();
+            t.x = x;
+            t.y = y;
+            return t;
+        }
+    public void Show()
+    {
+        Console.WriteLine("x {0}, y {1}", x, y);
+    }
+  }
+
 namespace _13_ObInitil
 {
     class Program
@@ -24,6 +40,12 @@ namespace _13_ObInitil
             
             Console.WriteLine(Rita.age);
             Console.WriteLine(IsAdult(baseAge: 18, age: 26));
+
+            for (int i=0; i<10; i++)
+            {
+                myClass my = myClass.Factory(i, i*2);
+                my.Show();                
+            }
             Console.ReadKey();
         }
     }
