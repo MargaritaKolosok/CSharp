e7e43a3bb8fe39382a9ee2a89920ad28ec393dc2
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-08 22:36:53 +0200

    calculator static

---
+++ EssentialLessons/Lesson_6/Task_1/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+/*
+Создать статический класс Calculator,
+с методами для выполнения основных арифметических операций.
+Написать программу, которая выводит на экран основные арифметические операции.  */
+static class Calculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }  
+}
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine(Calculator.Add(4, 5));
+            Console.ReadKey();
+        }
+    }
+}
