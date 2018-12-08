230b2426cab026031aa34bc7e32c5ed0522985b2
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-08 10:37:58 +0200

    matrix

---
+++ EssentialLessons/Lesson_5/Task_3/Task_3/MyMatrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3
+{
+    class MyMatrix
+    {
+        int[][] matrix;
+        public MyMatrix(int x, int y)
+        {
+            matrix = new int[x][];
+        }
+
+    }
+}
