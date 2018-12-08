be3be19da02ae360d6cf44779eff4ced6173b261
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-08 15:29:07 +0200

    store

---
+++ EssentialLessons/Lesson_5/Task_4/Task_4/Article.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+Создать класс Article, содержащий следующие закрытые поля:
+• название товара;
+• название магазина, в котором продается товар;
+• стоимость товара в гривнах.
+ * 
+ * 
+*/
+ 
+namespace Task_4
+{
+    class Article
+    {
+        private string product;
+        private string shop;
+        private double price;
+
+        public string Product
+        {
+            set { product = value; }
+            get { return product; }
+        }
+        public string Shop
+        {
+            set { shop = value; }
+            get { return shop; }
+        }
+        public double Price
+        {
+            set {
+
+                if (value > 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    Console.WriteLine("Price can not be 0 or less then 0");
+                }
+                
+                }
+            get { return price; }
+        }
+        public Article(string product, string shop, double price)
+        {
+            Product = product;
+            Shop = shop;
+            Price = price;
+        }
+
+        public string Info()
+        {
+            return string.Format("Product: {0}, Shop: {1}, Price: {2}", Product, Shop, Price);
+        }
+        
+    }
+}
---
+++ EssentialLessons/Lesson_5/Task_4/Task_4/Store.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*Создать класс Store, содержащий закрытый массив элементов типа Article.
+Обеспечить следующие возможности:
+• вывод информации о товаре по номеру с помощью индекса;
+• вывод на экран информации о товаре, название которого введено с клавиатуры,
+если таких товаров нет, выдать соответствующее сообщение;*/
+namespace Task_4
+{
+    class Store
+    {
+        Article[] storeArr;
+
+        public Store(int n)
+        {
+            storeArr = new Article[n];
+
+            for (int i=0; i<n; i++)
+            {
+                Console.WriteLine("Введите имя товара {0}", i);
+                string name = Convert.ToString(Console.ReadLine());
+                storeArr[i].Product = name;
+
+                Console.WriteLine("Введите магазин {0}", i);
+                string shop  = Convert.ToString(Console.ReadLine());
+                storeArr[i].Shop = shop;
+
+                Console.WriteLine("Введите цену товара {0}", i);
+                double price = Convert.ToDouble(Console.ReadLine());
+                storeArr[i].Price = price;
+            }       
+        }
+
+        public string this[string product]
+        {
+            get
+            { 
+                for (int i = 0; i < storeArr.Length; i++)
+                {
+                    if (storeArr[i].Product == product)
+                        return storeArr[i].Info();
+                }
+
+                return string.Format("\"{0}\" нет в наличии.", product);
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index < storeArr.Length && index >=0)
+                {
+                    return storeArr[index].Info();
+                }
+                return "Попытка обращения за пределы массива.";
+            }
+        }
+    }
+}
