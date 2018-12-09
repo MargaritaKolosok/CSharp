d86c31b0b099688f146f14cf5b5476275151fb36
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-09 09:16:56 +0200

    Update Program.cs

--- EssentialLessons/Lesson_6/Task_1/Task_1/Program.cs
+++ EssentialLessons/Lesson_6/Task_1/Task_1/Program.cs
@@ -5,10 +5,25 @@
 Написать программу, которая выводит на экран основные арифметические операции.  */
 static class Calculator
 {
-    public static int Add(int a, int b)
+    public static double Add(double a, double b)
     {
         return a + b;
-    }  
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+    public static double Sub(double a, double b)
+    {
+        return a - b;
+    }
+    public static double Div(double a, double b)
+    {
+        if (b == 0) { return 0; }
+        return a / b;
+    }
+
 }
 namespace Task_1
 {
@@ -17,6 +32,11 @@ namespace Task_1
         static void Main(string[] args)
         {            
             Console.WriteLine(Calculator.Add(4, 5));
+            Console.WriteLine(Calculator.Multiply(4, 5));
+            Console.WriteLine(Calculator.Multiply(4, 0));
+            Console.WriteLine(Calculator.Sub(4, 5));
+            Console.WriteLine(Calculator.Div(4, 5));
+            Console.WriteLine(Calculator.Div(4, 0));
             Console.ReadKey();
         }
     }
