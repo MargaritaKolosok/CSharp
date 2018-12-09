2e15675a74b642c26c489d9e02c98f69b5a0b5c8
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-09 12:24:48 +0200

    n

---
+++ EssentialLessons/Lesson_6/Task_2/Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+Создать статический класс FindAndReplaceManager
+с методом void FindNext(string str) для поиска по книге из примера урока 005_Delegation.
+При вызове этого метода, производится последовательный поиск строки в книге
+*/
+
+class Book
+{
+    string book = "";
+    public Book(string str)
+    {
+        if (book!="")
+        {
+            book +=" /n" + str;
+        }
+        book = str;
+    }
+}
+static class FindAndReplaceManager
+{
+    public void FindNext(string str)
+    {
+
+    }
+}
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
