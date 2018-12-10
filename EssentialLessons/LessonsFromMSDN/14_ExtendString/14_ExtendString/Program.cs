d7ed6287aabab1a52dcf49f53aea8311166f284a
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-10 12:45:34 +0200

    string extanded

---
+++ EssentialLessons/LessonsFromMSDN/14_ExtendString/14_ExtendString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+static class String
+{
+    public static string Revert(this string str)
+    {
+        string result = "";
+        int length = str.Length - 1;
+        foreach (char ch in str)
+        {
+            result += str[length];
+            length--;
+        }
+        return result;
+    }
+}
+namespace _14_ExtendString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Hello my dear frieds";
+            Console.WriteLine(test.Revert());
+            
+            Console.ReadKey();
+        }
+    }
+}
