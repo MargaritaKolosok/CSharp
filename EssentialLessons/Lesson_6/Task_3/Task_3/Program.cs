5fb7bfd7d9ce9f739a90448b72353dc65cde3048
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-10 11:28:36 +0200

    subclass

---
+++ EssentialLessons/Lesson_6/Task_3/Task_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Book
+{ 
+  public  class Note
+    {
+        string note = "";
+
+        public Note(string str)
+        {
+            note += str;
+        }
+        public string Text
+        {
+            get;
+            set;
+        }
+    }
+
+    public void FindText(string str)
+    {
+        Console.WriteLine(str + "/n");
+    }
+}
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();           
+            Book.Note v = new Book.Note("Book note");
+
+            v.Text = "Text in note subclass";
+            b.FindText(v.Text);
+            Console.ReadKey();
+        }
+    }
+}
