1b2091cd3bcf3219d2f8a612aeaecd6eebe7a6c4
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-11 09:13:13 +0200

    nested

---
+++ EssentialLessons/LessonsFromMSDN/NestedClass/NestedClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Container
+{   int x;
+    public Container()
+    {
+        x = 10;
+    }
+    public int X
+    {
+        get { return x; }
+    }
+   public class Nested
+    {
+        private Container parent;
+
+        public Nested(Container parent)
+        {
+            this.parent = parent;
+        }
+        public int GetX()
+        {
+            return parent.X;
+        }
+    }
+}
+namespace NestedClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Container c = new Container();
+            Container.Nested my = new Container.Nested(c);
+            Console.WriteLine(my.GetX());
+            Console.ReadKey();
+        }
+    }
+}
