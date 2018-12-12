ad199c1c07efd4b6dab5dbb1d59f064c415c37a0
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-12 08:53:11 +0200

    struct

---
+++ EssentialLessons/LessonsFromMSDN/16_Symb/16_Symb/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Введите массив символов из 10 элементов. Замените символы-цифры на
+символ ' * ' 
+
+ */
+class Sequrity
+{
+    public static string[] Secure(string[] arr)
+    {
+        string[] secure = new string[arr.Length];
+        for (int i=0; i<arr.Length; i++)
+        {
+            if (int.TryParse(Convert.ToString(arr[i]), out int result))
+            {
+                secure[i] = "#";
+            }
+            else
+            {
+                secure[i] = arr[i];
+            }
+        }
+       
+        return secure;
+    }
+}
+
+
+namespace _16_Symb
+{
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] my = new string[];
+        }
+    }
+}
---
+++ EssentialLessons/LessonsFromMSDN/Structures/Structures/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+struct Book
+{
+    public string Name;
+    public int Pages;
+
+    public Book(string s, int i)
+    {
+        Name = s;
+        Pages = i;
+    }
+}
+class Magazine
+{
+    public string Name;
+    public int Pages;
+
+    public Magazine(string s, int i)
+    {
+        Name = s;
+        Pages = i;
+    }
+    public Magazine()
+    {
+
+    }
+}
+namespace Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Book1 name", 256);
+            Book book2 = new Book();
+            book2 = book1;
+
+            Console.WriteLine(book1.Name + " " + book2.Name);
+            book1.Name = "Book 1 name changed";
+            Console.WriteLine(book1.Name + " " + book2.Name);
+
+            Magazine mag1 = new Magazine("Magazine name", 256);
+            Magazine mag2 = new Magazine();
+            mag2 = mag1;
+
+            Console.WriteLine(mag1.Name + " " + mag2.Name);
+            mag1.Name = "Magazine1 name changed";
+            Console.WriteLine(mag1.Name + " " + mag2.Name);
+            Console.ReadKey();
+
+        }
+    }
+}
