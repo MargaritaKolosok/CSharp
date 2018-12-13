9bbeac804456ca31ca461f85ea592cc0d769fc7c
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-13 16:33:12 +0200

    Create Train.cs

---
+++ EssentialLessons/New folder/Task_2/Task_2/Train.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    struct Train
+    {
+        string city;
+        int number;
+        DateTime time;
+
+        public Train(string city, int number, DateTime time)
+        {
+            this.city = city;
+            this.number = number;
+            this.time = time;
+        }
+
+        public string City
+        {
+            get { return city; }
+        }
+        public int Numver
+        {
+            get { return number; }
+
+        }
+        public DateTime Time
+        {
+            get { return time; }
+        }        
+    }
+}
