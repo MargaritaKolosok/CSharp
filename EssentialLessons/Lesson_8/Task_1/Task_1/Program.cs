1310fe6babc846186ce5489a337c63a7fe58cc1b
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-15 15:12:01 +0200

    colors

---
+++ EssentialLessons/Lesson_8/Task_1/Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+/*
+Используя Visual Studio, создайте проект по шаблону Console Application.
+Создайте статический класс с методом void Print (string stroka, int color), который выводит на
+экран строку заданным цветом. Используя перечисление, создайте набор цветов, доступных
+пользователю. Ввод строки и выбор цвета предоставьте пользователю. 
+ * */
+static class MyClass
+{
+    enum colors {Blue, Red, Green, White};
+    static public void Print(string str, int color)
+    {
+        
+    }
+
+}
+
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
