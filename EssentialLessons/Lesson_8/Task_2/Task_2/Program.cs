a402e2308d534bb5aef6215518eacaa12e89323e
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-15 23:39:12 +0200

    days to birthday

---
+++ EssentialLessons/Lesson_8/Task_2/Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+/*
+Используя Visual Studio, создайте проект по шаблону Console Application.  
+Реализуйте программу, которая будет принимать от пользователя дату его рождения и выводить количество дней до его следующего дня рождения. 
+ 
+ * */
+static class CountDays
+{
+    public static void DaysToBirthday(string str)
+    {
+        if (DateTime.TryParse(str, out DateTime Birthday))
+        {
+            DateTime today = DateTime.Now;
+            TimeSpan DaysToBirthday = today - Birthday;
+            Console.WriteLine("Days left to Birthday {0}", DaysToBirthday);
+        }
+        else
+        {
+            Console.WriteLine("Not valid Day entered!");
+        }
+       
+    }
+}
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Date of birth");
+            string birth = Console.ReadLine();
+            CountDays.DaysToBirthday(birth);
+            Console.ReadKey();
+        }
+    }
+}
