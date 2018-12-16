150c1ce6fc1bcfeff2f2ba460876c0a182b87531
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-16 18:19:22 +0200

    year

---
+++ EssentialLessons/LessonsFromMSDN/Date/Date/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Date
+{
+    class Program
+    {
+        static class Year
+        {
+            public static void CheckYear(DateTime year)
+            {
+                int Year = year.Year;
+                bool result = (Year % 4 ==0) ? true: false;
+                if (result)
+                    Console.WriteLine("God {0} - visokosnij", Year);
+                else
+                    Console.WriteLine("God {0} - ne visokosnij", Year);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year");
+            DateTime x = DateTime.Parse(Console.ReadLine());
+            Year.CheckYear(x);
+            Console.ReadKey();
+        }
+    }
+}
