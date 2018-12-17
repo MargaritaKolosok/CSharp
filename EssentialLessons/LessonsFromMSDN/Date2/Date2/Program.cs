6b22e15753d3a296305c923d8ecd398b82b7fe41
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-17 13:16:50 +0200

    days to new year

---
+++ EssentialLessons/LessonsFromMSDN/Date2/Date2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan daysToNewYear;
+
+            DateTime NewYear = new DateTime(now.Year, 1, 1);
+
+            if (now > NewYear)
+            {
+                NewYear = new DateTime(now.Year + 1, 1, 1);
+                daysToNewYear = NewYear - now;
+            }
+            else
+            {
+                daysToNewYear = NewYear - now;
+            }
+            Console.WriteLine(" daysToNewYear {0}", daysToNewYear.Days);
+            Console.ReadKey();
+        }
+    }
+}
