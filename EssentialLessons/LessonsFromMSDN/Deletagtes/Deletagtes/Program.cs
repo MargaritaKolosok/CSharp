6455b4d573146809b5e5190e9366ad31960305df
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-20 11:33:34 +0200

    delegate

---
+++ EssentialLessons/LessonsFromMSDN/Deletagtes/Deletagtes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deletagtes
+{
+    class Program
+    {
+        public delegate void Message(string str);
+
+        public static void Display(string str)
+        {
+            System.Console.WriteLine(str);
+        }
+        static void Main(string[] args)
+        {
+            Message message = Display;
+            Display("Hello world!");
+            Console.ReadKey();
+            
+        }
+    }
+}
