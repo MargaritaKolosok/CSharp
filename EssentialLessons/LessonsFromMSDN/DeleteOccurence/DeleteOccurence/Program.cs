016afb50789c8de818e34abec2e8c86f7dd0461d
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-21 12:57:10 +0200

    #1

---
+++ EssentialLessons/LessonsFromMSDN/DeleteOccurence/DeleteOccurence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public static int[] DeleteNth(int[] arr, int x)
+{
+    List<int> list = new List<int> { };
+
+    for (int i=0; i<arr.Length; i++)
+    {
+        int count = 0;
+        for (int j=0; j<arr.Length; j++)
+        {
+            if (count<x)
+            {
+                list.Add(arr[i]);
+            }
+        }
+    }
+}
+namespace DeleteOccurence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ EssentialLessons/LessonsFromMSDN/MAxValueSeq/MAxValueSeq/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace MAxValueSeq
+{
+   public static class MyExtensions
+    {
+        public static void GetMax(this string str)
+        {
+            int max = Convert.ToInt32(str.Substring(0, 4));
+            Console.WriteLine(max);
+            
+            for (int i = 1; i <= str.Length - 5; i++)
+            {
+                int num1 = Convert.ToInt32(str.Substring(i, (int)(i + 4)));
+                if (max < num1)
+                {
+                    max = num1;
+                }
+            }
+
+            Console.WriteLine("Max sequence is {0}", max);
+            //return max;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "7316717653133062491922511967442657443";
+            str.GetMax();
+            Console.ReadKey();
+        }
+    }
+}
