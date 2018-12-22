464b37435ae23ef788f3f8bff5bb0e7486dae518
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-22 15:36:36 +0200

    delegate

---
+++ EssentialLessons/Lesson_9/Task_1/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public delegate int PerformCalculation(int a, int b, int c);
+
+        public static int Calculation(int a, int b, int c)
+        {
+            int result = (a + b + c) / 3;
+            
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            PerformCalculation handler = Calculation;
+            
+            Console.WriteLine("Result is {0}", handler(1, 2, 3));
+            Console.ReadKey();
+
+        }
+    }
+}
