4d2f6b314adc8fae16fecee0c8f43ad365080955
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-22 22:22:28 +0200

    delegate

---
+++ EssentialLessons/Lesson_9/Task_3/Task_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+public delegate int myDelegate(Num[] arr);
+
+public delegate int Num();
+
+namespace Task_3
+{   
+    class Program
+    {
+        public static int Random1()
+        {
+            Random random = new Random();
+            return random.Next(1,10);
+        }
+        public static int Random2()
+        {
+            Random random = new Random();
+            return random.Next(1, 10);
+        }
+        public static int Random3()
+        {
+            Random random = new Random();
+            return random.Next(1, 10);
+        }
+
+        public static int Avarage(int[] arr)
+        {
+            int sum = 0;
+            for (int i=0; i<arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum / arr.Length;
+        }
+
+        static void Main(string[] args)
+        {
+            Num[] num = new Num[] { Random1, Random2, Random3 };
+            
+            myDelegate my = Avarage(num);
+
+
+        }
+    }
+}
