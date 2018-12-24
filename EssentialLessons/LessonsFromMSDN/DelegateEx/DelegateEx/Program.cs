34ece5684c265c839b25bf9c77cfe364f0c47264
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-24 23:23:22 +0200

    delegate

---
+++ EssentialLessons/LessonsFromMSDN/DelegateEx/DelegateEx/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegateEx
+{
+    delegate double MathDelegate(double x);
+
+    class Program
+    {
+        static double Double(double x)
+        {
+            return x * 2;
+        }
+
+        static void Main(string[] args)
+        {
+            MathDelegate math = Double;
+            double y = math(3);
+
+            Console.WriteLine(math);
+            Console.ReadKey();
+        }
+    }
+}
