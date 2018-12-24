8deab6642ca17611c420fb51dfeb0c4c373e317f
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-25 00:10:06 +0200

    delegate str

---
+++ EssentialLessons/LessonsFromMSDN/Delegate/Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegate
+{
+    delegate string ChangeString(string str);
+
+    class Program
+    {
+        static string UpperCase(string str)
+        {           
+            return str.ToUpper();
+        }
+
+        static string LowerCase(string str)
+        {
+            return str.ToLower();
+        }
+
+        static void Main(string[] args)
+        {
+            ChangeString StringChanger = UpperCase;
+            string test = "Hello world!";
+            Console.WriteLine(StringChanger(test));
+
+            StringChanger = LowerCase;
+            Console.WriteLine(StringChanger(test));
+
+            StringChanger += UpperCase;
+            Console.WriteLine(StringChanger(test));
+
+            Console.ReadKey();
+
+        }
+    }
+}
