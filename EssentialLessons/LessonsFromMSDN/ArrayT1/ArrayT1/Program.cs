5b3f289838c846ca72c39051ff1dfb3b84c8f31c
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-24 23:37:04 +0200

    array

---
+++ EssentialLessons/LessonsFromMSDN/ArrayT1/ArrayT1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ArrayT1
+{   
+    class Program
+    {
+        static int[,] ArrayGenerator(int x, int y)
+        {
+            int[,] array = new int[x, y];
+            Random random = new Random();
+            for (int i=0; i<x; i++)
+            {
+                for (int j=0; j<y; j++)
+                {
+                    array[i, j] = random.Next(1,100);
+                }
+            }
+            return array;
+            
+        }
+
+        static void Main(string[] args)
+        {
+            
+            int[,] arr = ArrayGenerator(5, 5);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    Console.Write(arr[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
--- EssentialLessons/LessonsFromMSDN/DelegateEx/DelegateEx/Program.cs
+++ EssentialLessons/LessonsFromMSDN/DelegateEx/DelegateEx/Program.cs
@@ -16,7 +16,19 @@ namespace DelegateEx
             MathDelegate math = Double;
             double y = math(3);
 
-            Console.WriteLine(math);
+            Console.WriteLine(y);
+
+            MathDelegate math2 = delegate (double x) { return x * x; };
+
+            double z = math2(4);
+            Console.WriteLine(z);
+
+            MathDelegate math3 = x => x * x * x;
+
+            double c = math3(5);
+            Console.WriteLine(c);
+
+
             Console.ReadKey();
         }
     }
