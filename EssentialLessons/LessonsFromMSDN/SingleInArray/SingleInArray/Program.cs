fa9d2c4349a33dbca09a8a2c19b40664817f0674
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-25 01:31:29 +0200

    Update Program.cs

--- EssentialLessons/LessonsFromMSDN/SingleInArray/SingleInArray/Program.cs
+++ EssentialLessons/LessonsFromMSDN/SingleInArray/SingleInArray/Program.cs
@@ -2,38 +2,42 @@
 
 namespace SingleInArray
 {
-    class Program
-    {
+  class Program
+   {
         public static int Stray(int[] array)
         {
-            int one;
-            one = array[0];
-            for (int i=0; i< array.Length; i++)
+            int uniq = array[0];
+
+            for (int i=0; i < array.Length; i++)
             {
-                
                 for (int j=0; j<array.Length; j++)
                 {
-                    if (j != i)
+                    if (i==j)
                     {
-                        if (one == array[j])
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            one = array[i];
-                        }
+                        continue;
                     }
-                   
+                    if (array[i] == array[j])
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        uniq = array[i];
+                    }
                 }
             }
-            return one;
+
+            return uniq;
+
         }
+            
+        
         static void Main(string[] args)
         {
-            int[] test = new int[] { 1,1,1,1,2,1,1,1,1};
+            int[] test = new int[] { 1,2,1,1,1,1,1,1,1};
             int result = Stray(test);
             Console.WriteLine(result);
+
             Console.ReadKey();
         }
     }
