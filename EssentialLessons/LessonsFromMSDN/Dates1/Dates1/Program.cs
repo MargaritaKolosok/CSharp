784b192f45e658c33273d9a6bd32279b0e598eed
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-27 16:03:58 +0200

    delegate

--- EssentialLessons/LessonsFromMSDN/Dates1/Dates1/Program.cs
+++ EssentialLessons/LessonsFromMSDN/Dates1/Dates1/Program.cs
@@ -34,21 +34,21 @@ class Person
         this.phone = phone;
     }
 
-
 }
 
 public static class ExtensionMethods
 {
-    public static TimeSpan DaysToBirthday(this DateTime birthday)
+    public static int DaysToBirthday(this DateTime birthday)
     {
         DateTime now = DateTime.Now;
         DateTime birth = new DateTime(now.Year, birthday.Month, birthday.Day);
+
         if (now > birth)
         {            
             DateTime birth2 = new DateTime(now.Year+1, birthday.Month, birthday.Day);
-            return  now.Date - birth2.Date;
+            return  (int)(birth2 - now).TotalDays;
         }
-         return (now.Date - birth.Date);
+         return (int)(birth - now).TotalDays;
     }
 }
 
@@ -58,15 +58,14 @@ namespace Dates1
     {
         static void Main(string[] args)
         {
-            DateTime day = new DateTime(1992,06,28);
+            DateTime day = new DateTime(1992,12,28);
             
             Person me = new Person("Rita", day, "+398298938938");
             Console.WriteLine(me.Birthday);
             Console.WriteLine(me.Name);
             Console.WriteLine(me.Phone);
 
-            Console.WriteLine(day.DaysToBirthday());
-            
+            Console.WriteLine(day.DaysToBirthday());            
 
             Console.ReadKey();
         }
---
+++ EssentialLessons/LessonsFromMSDN/delegate2/delegate2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace delegate2
+{
+    
+    class Program
+    {
+        delegate string StrMethod(string str);
+
+        class DelegateTest
+        {
+            static string Reverce(string str)
+            {
+                string text = "";
+
+                for (int i = str.Length - 1; i == 0; i--)
+                {
+                    text += str[i];
+                }
+                return text;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           // DelegateTest test = new DelegateTest();
+            StrMethod MakeReverce = new StrMethod(Reverce);
+        }
+    }
+}
