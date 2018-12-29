b4b6d7347cdc123bd72fcf8423aa99d5740eaa97
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2018-12-29 23:00:23 +0200

    event

---
+++ EssentialLessons/LessonsFromMSDN/Events/Events/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+delegate void myEventHandler();
+
+class MyEvent
+{
+    public event myEventHandler SomeEvent;
+
+    public void OnSomeEvent()
+    {
+        if (SomeEvent!=null)
+        {
+            SomeEvent();
+        }
+    }    
+}
+class EventDemo
+{
+    public static void Handler()
+    {
+        Console.WriteLine("This Event fired!");
+    }
+}
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyEvent newEvent = new MyEvent();
+            newEvent.SomeEvent += EventDemo.Handler;
+            newEvent.OnSomeEvent();
+            Console.ReadKey();
+        }
+    }
+}
