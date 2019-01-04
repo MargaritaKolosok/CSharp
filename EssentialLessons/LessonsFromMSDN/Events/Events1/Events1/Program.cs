ee7a264d04d1cc1c8df24e3039367818a1fb7990
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-04 09:24:32 +0200

    #events

---
+++ EssentialLessons/LessonsFromMSDN/Events/Events1/Events1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events1
+{ 
+    class Counter
+    {
+        public delegate void MethodContainer();
+        
+        public void Count()
+        {
+            for (int i=0; i<100; i++)
+            {
+                if (i=-50)
+                {
+
+                }
+            }
+        }
+    }
+    class Handler_1
+    {
+        public void Message()
+        {
+            Console.WriteLine("Handler 1 reaction");
+        }
+    }
+    class Handler_2
+    {
+        public void Message()
+        {
+            Console.WriteLine("Handler 2 reaction");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
