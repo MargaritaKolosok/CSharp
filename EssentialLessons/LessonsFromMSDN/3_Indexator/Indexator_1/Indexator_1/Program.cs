970ce79260db8f549f501275cf4b510751430a7e
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-05 11:40:15 +0200

    indexator

---
+++ EssentialLessons/LessonsFromMSDN/3_Indexator/Indexator_1/Indexator_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Indexator_1
+{
+    class Actor
+    {
+        string[] Roles = { "Mother", "Doughter", "Witch", "Queen" };
+
+        public string this[int x]
+        {
+            get
+            {
+                if (x >= 0 && x < Roles.Length)
+                {
+                    return Roles[x];
+                }
+                else
+                {
+                    return string.Format("No such role");
+                }
+            }
+        }
+            
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Actor actor = new Actor();
+            Console.WriteLine(actor[1]);
+            Console.WriteLine(actor[3]);
+            Console.WriteLine(actor[5]);
+            Console.ReadKey();
+        }
+    }
+}
