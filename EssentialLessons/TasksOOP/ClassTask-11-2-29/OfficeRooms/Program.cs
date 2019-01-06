9b64106085b90325a0b1769b88044db11e9926dc
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-06 12:56:03 +0200

    OfficeRooms

---
+++ EssentialLessons/TasksOOP/ClassTask-11-2-29/OfficeRooms/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace OfficeRooms
+{
+    class Room
+    {
+        protected double height;
+        protected double width;
+        protected double length;
+
+        public Room(double height, double width, double length)
+        {
+            this.height = height;
+            this.width = width;
+            this.length = length;
+        }
+        public double CeilingArea()
+        {
+            return width * length;
+        }
+        public double WallsArea()
+        {
+            return 2 * (height * length) + 2 * (width * height);
+        }
+    }
+    class Office
+    {
+        int room;
+        double paintAmount;
+
+        Room[] RoomsArray;
+
+        public Office(int room, double paintAmount)
+        {
+            this.room = room;
+            this.paintAmount = paintAmount;
+
+            CreateRooms();
+        }
+
+        private void CreateRooms()
+        {
+            for (int i=0; i<room; i++)
+            {
+                Room r;
+                Console.WriteLine("Room Width");
+                double w = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Room height");
+                double h = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Room Length");
+                double l = Convert.ToDouble(Console.ReadLine());
+
+                r = new Room(w,h,l);
+
+                RoomsArray[i] = r;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
