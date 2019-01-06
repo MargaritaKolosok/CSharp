2a7749b06b3dc90ca0311b011c159862d920bfea
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-06 12:07:13 +0200

    Exceptions

---
+++ EssentialLessons/TasksOOP/Exceptions_2/Exceptions_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Exceptions_2
+{
+    class Program
+    {
+        class ExceptA : Exception
+        {
+            public ExceptA(string str) : base(str)
+            {
+
+            }
+            public override string ToString()
+            {
+                return Message;
+            }
+        }
+        class ExceptB : ExceptA
+        {
+            public ExceptB(string str) : base(str) { }
+            public override string ToString()
+            {
+                return Message;
+            }
+        }
+        static void Main(string[] args)
+        {
+            for (int i=0; i<3; i++)
+            {
+                try
+                {
+                    if (i == 0)
+                    {
+                        throw new ExceptA("Перехват исключения типа ExceptA");
+                    }
+                    if (i == 1)
+                    {
+                        throw new ExceptB("Перехват исключения типа ExceptB");
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch(ExceptB exc)
+                {
+                    Console.WriteLine(exc);
+                }
+                catch (ExceptA exc)
+                {
+                    Console.WriteLine(exc);
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
