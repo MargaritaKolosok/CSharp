f36a883c8d0c2b051210fda0716b4760fb44753e
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-07 21:07:17 +0200

    Selenium installed

---
+++ Selenium/UnitTestProject1/UnitTestProject1/MyTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MyTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.google.com/";
+            
+        }
+    }
+}
