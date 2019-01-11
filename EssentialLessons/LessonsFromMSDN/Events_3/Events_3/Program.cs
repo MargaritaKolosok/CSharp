7486e8245f38c0b6c62e8c6703a0f1eb05ae7d88
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-11 09:53:35 +0200

    #

---
+++ EssentialLessons/LessonsFromMSDN/Events_3/Events_3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_3
+{
+    public delegate void MyDelegate();
+
+    class Test
+    {
+        public virtual void Message()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+    class Test2 : Test
+    {
+        public override void Message()
+        {
+            Console.WriteLine("Test2");
+        }
+    }
+
+    class Program
+    {
+        public static void Method()
+            {
+
+            Console.WriteLine("Hello");
+
+            }
+
+
+        static void Main(string[] args)
+        {
+            Test2 my = new Test2();
+            MyDelegate test = my.Message;
+            test();
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
