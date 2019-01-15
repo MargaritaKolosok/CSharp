a96dfa4654e67fd03103c2c7f407ec7688593d5f
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-15 09:46:44 +0200

    datetime

---
+++ EssentialLessons/TasksOOP/Task1/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2019, 6, 6);
+            TimeSpan timeSpan = new TimeSpan();
+            DateTime now = DateTime.Now;
+
+            timeSpan = date - now;
+
+            Console.WriteLine(timeSpan.Days);
+
+            Console.ReadKey();
+        }
+    }
+}
