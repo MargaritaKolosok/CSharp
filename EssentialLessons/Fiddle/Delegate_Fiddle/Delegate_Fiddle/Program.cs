f89d96598c5e7964f905b7e09da1f63ebce6ae19
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-15 19:50:43 +0200

    delegates

---
+++ EssentialLessons/Fiddle/Delegate_Fiddle/Delegate_Fiddle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Fiddle
+{
+    class Program
+    {
+        public delegate int MathPow(int x);
+
+        private static int Pow2(int x)
+        {
+            Console.WriteLine("pow2");
+            return x * x;
+            
+        }
+        private static int Pow3(int x)
+        {
+            Console.WriteLine("pow3");
+            return x * x * x;
+        }
+
+        static void Main(string[] args)
+        {
+            MathPow pow2 = Pow2;
+            MathPow pow3 = Pow3;
+            Console.WriteLine(pow2(2));
+            Console.WriteLine(pow3(2));
+
+            pow2 += Pow3;
+            Console.WriteLine(pow2(2));
+
+            Console.ReadKey();
+           
+        }
+    }
+}
---
+++ EssentialLessons/LessonsFromMSDN/ConsoleTest/ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public delegate void myDelegate(string str);
+
+        static void Message(string str)
+        {
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Message("Hello");
+        }
+    }
+}
