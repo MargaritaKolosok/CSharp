b6e8116d5d45eea997e363c9e9caac310a8a5505
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-16 22:07:11 +0200

    events

---
+++ EssentialLessons/Fiddle/Events_Fiddle/EventsFiddle1/EventsFiddle2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFiddle2
+{
+    public delegate void MyDelegate();
+
+    public class myClass
+    {
+        MyDelegate myEvent = null;
+
+        public event MyDelegate MyEvent
+        {
+            add
+            {
+                myEvent += value;
+            }
+            remove
+            {
+                myEvent -= value;
+            }
+        }
+
+        public void InvokeEvent()
+        {
+            if (myEvent == null)
+            {
+                Console.WriteLine("Nothing attached to event");
+            }
+            else
+            {
+                myEvent.Invoke();
+            }
+            
+        }
+    }
+    class Program
+    {
+        public static void Method1()
+        {
+            Console.WriteLine("Hey");
+        }
+        static void Main(string[] args)
+        {
+            myClass instance = new myClass();
+            instance.MyEvent += Method1;
+
+            instance.InvokeEvent();
+
+            instance.MyEvent -= Method1;
+            instance.InvokeEvent();
+
+            Console.ReadKey();
+        }
+    }
+}
