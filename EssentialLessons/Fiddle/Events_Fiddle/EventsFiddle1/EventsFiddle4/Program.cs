9a4335eb1a26e025cda42206a71d6fb35ec226cc
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-19 22:31:09 +0200

    events

---
+++ EssentialLessons/Fiddle/Events_Fiddle/EventsFiddle1/EventsFiddle4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFiddle4
+{
+    public delegate void Contact(string contact);
+
+    class ContactBook
+    {
+        List<string> Book;
+        public void AddToList(string str)
+        {
+            Book.Add(str);
+        }
+        event Contact contactAdded;
+        event Contact deleteContact;
+
+        public event Contact ContactAdded
+        {
+            add
+            {
+                contactAdded += value;
+            }  
+            remove
+            {
+
+            }
+        }
+
+        public event Contact ContactDeleted
+        {
+            add { }
+            remove
+            {
+                deleteContact -= value;
+            }
+        }
+    }
+    
+    class Program
+    {
+        public static void ContactMethod(string str)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
