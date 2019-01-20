76aab64356e79c560a6a3be2039a1d3a758b1e3c
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-20 19:07:38 +0200

    events

---
+++ EssentialLessons/Fiddle/Events_Fiddle/EventsFiddle5/EventsFiddle6/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFiddle6
+{
+    class KeyEventArgs : EventArgs
+    {
+        public char ch;
+    }
+    class EventKey
+    {
+        public event EventHandler<KeyEventArgs> KeyPress;
+
+        public void OnKeyPress(char key)
+        {
+            KeyEventArgs k = new KeyEventArgs();
+
+            if (KeyPress!= null)
+            {
+                k.ch = key;
+                KeyPress(this, k);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventKey keyEvent = new EventKey();
+
+            ConsoleKeyInfo key;
+
+            int count = 0;
+
+            keyEvent.KeyPress += (sender, e) => Console.WriteLine("Get info about pressing key {0}", e.ch);
+            keyEvent.KeyPress += (sender, e) => count++;
+
+            Console.WriteLine("To finish programm press '.'");
+
+            do
+            {
+                key = Console.ReadKey();
+                keyEvent.OnKeyPress(key.KeyChar);
+            }
+            while (key.KeyChar != '.');
+
+            Console.WriteLine($"You've pressed {count} keys");
+            Console.ReadKey();
+        
+        }
+    }
+}
