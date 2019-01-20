a88629fcb58df4f69ad538b467f5a40c4dfdd66d
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-20 20:13:41 +0200

    events Task from Video lesson

---
+++ EssentialLessons/Lesson12/Task_1/Task_1/Presenter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Presenter
+    {
+        Model model;
+        MainWindow mainWindow;
+
+        public Presenter(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+            this.model = new Model();
+            this.mainWindow.SomeEvent += MainWindow_SomeEvent;
+        }
+
+        private void MainWindow_SomeEvent(object sender, EventArgs e)
+        {
+            this.mainWindow.textBox1.Text = this.model.SomeMethod(this.mainWindow.textBox1.Text);
+        }
+    }
+}
