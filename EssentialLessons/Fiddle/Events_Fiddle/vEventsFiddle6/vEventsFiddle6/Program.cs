884552d1ce71f23551b62a84c26a2052b4b3708c
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-22 11:39:37 +0200

    Update Program.cs

--- EssentialLessons/Fiddle/Events_Fiddle/vEventsFiddle6/vEventsFiddle6/Program.cs
+++ EssentialLessons/Fiddle/Events_Fiddle/vEventsFiddle6/vEventsFiddle6/Program.cs
@@ -25,7 +25,7 @@ namespace vEventsFiddle6
     class Counter
     {
        private int threshold;
-       private int total;
+       private int total=0;
 
         public Counter(int threshold)
         {
@@ -40,22 +40,42 @@ namespace vEventsFiddle6
                 ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
                 args.Threshold = threshold;
                 args.TimeReached = DateTime.Now;
-
             }
         }
         protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
         {
-
+            EventHandler<ThresholdReachedEventArgs> handler = ThresholdReached;
+            ThresholdReached?.Invoke(this, e);
         }
 
         public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
     }
-    
-
+   
     class Program
     {
+        static void MethodThresholdReached(object sender, ThresholdReachedEventArgs e)
+        {
+            Console.WriteLine(sender);
+            Console.WriteLine($"{e.Threshold} was reached at {e.TimeReached}");
+            Environment.Exit(0);
+        }
+
         static void Main(string[] args)
         {
+            Counter counter = new Counter(5);
+            
+            counter.ThresholdReached += MethodThresholdReached;
+
+            Console.WriteLine("Press a to continue and incrice num");
+            while (Console.ReadKey(true).KeyChar =='a')
+            {
+                Console.WriteLine("Adding 1");
+                counter.Add(1);
+            }
+
+
+            Console.ReadKey();
+
         }
     }
 }
