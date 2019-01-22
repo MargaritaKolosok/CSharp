b74125b3f4024d1135255beadeafb705685b07b6
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-22 11:28:02 +0200

    Update Program.cs

--- EssentialLessons/Fiddle/Events_Fiddle/vEventsFiddle6/vEventsFiddle6/Program.cs
+++ EssentialLessons/Fiddle/Events_Fiddle/vEventsFiddle6/vEventsFiddle6/Program.cs
@@ -6,19 +6,51 @@ using System.Threading.Tasks;
 
 namespace vEventsFiddle6
 {
-    class EventHandlerArgs : EventArgs
+    class ThresholdReachedEventArgs : EventArgs
     {
-        public int Day
+        public int Threshold
         {
-            get { return Day; }
-            private set { Day = Date.DayOfYear; }
+            get;
+            set;
         }
-        public DateTime Date
+        public DateTime TimeReached
         {
-            get { return Date; }
-            set { Date = DateTime.Now; }
+            get;
+            set;
         }
     }
+
+    
+
+    class Counter
+    {
+       private int threshold;
+       private int total;
+
+        public Counter(int threshold)
+        {
+            this.threshold = threshold;
+        }
+
+        public void Add(int x)
+        {
+            total += x;
+            if (total >= threshold)
+            {
+                ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
+                args.Threshold = threshold;
+                args.TimeReached = DateTime.Now;
+
+            }
+        }
+        protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
+        {
+
+        }
+
+        public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
+    }
+    
 
     class Program
     {
