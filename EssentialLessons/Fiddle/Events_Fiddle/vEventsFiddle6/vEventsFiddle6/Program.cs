95506fc44b60b67eaf7c1d391d0b5eab4fa0751e
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-22 11:10:59 +0200

    events fiddle

---
+++ EssentialLessons/Fiddle/Events_Fiddle/vEventsFiddle6/vEventsFiddle6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vEventsFiddle6
+{
+    class EventHandlerArgs : EventArgs
+    {
+        public int Day
+        {
+            get { return Day; }
+            private set { Day = Date.DayOfYear; }
+        }
+        public DateTime Date
+        {
+            get { return Date; }
+            set { Date = DateTime.Now; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
