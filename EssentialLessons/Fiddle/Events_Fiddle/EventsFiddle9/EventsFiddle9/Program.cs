1eda1706c39c0df369872b06511977669793cf33
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-23 16:28:50 +0200

    Fiddle events

---
+++ EssentialLessons/Fiddle/Events_Fiddle/EventsFiddle9/EventsFiddle9/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFiddle9
+{
+    public class ShapeEventArgs : EventArgs
+    {
+        double newArea;
+
+        public ShapeEventArgs(double newArea)
+        {
+            this.newArea = newArea;
+        }
+        public double NewArea
+        {
+            get { return newArea; }
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected double area;
+
+        public double Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
+
+        public event EventHandler<ShapeEventArgs> ShapeChanged;
+
+        public abstract void Draw();
+
+        protected virtual void OnShapeChanged(ShapeEventArgs e)
+        {
+            EventHandler<ShapeEventArgs> handler = ShapeChanged;
+            handler?.Invoke(this, e);
+           
+        }
+    }
+
+    public class Circle : Shape
+    {
+        double radius;
+
+        double CircleArea(double radius)
+        {
+            return (3.14 * radius * radius);
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            area = CircleArea(radius);
+        }
+
+        public void Update(double radius)
+        {
+            this.radius = radius;
+            area = CircleArea(radius);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
