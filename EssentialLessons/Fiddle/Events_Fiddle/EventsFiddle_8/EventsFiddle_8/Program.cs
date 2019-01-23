fee0267f8c2a43a8a8d9473f49325e1b3e68c0d0
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-23 12:47:17 +0200

    Update Program.cs

--- EssentialLessons/Fiddle/Events_Fiddle/EventsFiddle_8/EventsFiddle_8/Program.cs
+++ EssentialLessons/Fiddle/Events_Fiddle/EventsFiddle_8/EventsFiddle_8/Program.cs
@@ -21,31 +21,65 @@ namespace EventsFiddle_8
         {           
             int numToGuess = (new Random()).Next(1,10);
             int userNum;
+            int tries = 1;
 
             Console.WriteLine("You have 3 tries to guess number from 1 to 10");
 
             for (int i=0; i<3; i++)
             {
-                Console.WriteLine($"Try #{i} Enter num:");
+                Console.WriteLine($"Try #{tries} Enter num:");
                 userNum = int.Parse(Console.ReadLine());
 
                 if (numToGuess == userNum)
                 {
-
+                    CustomEventArgs args = new CustomEventArgs();
+                    OnNumberGuessed(args);
                 }
+                if (tries >= 3 && numToGuess != userNum)
+                {
+                    CustomEventArgs args = new CustomEventArgs();
+                    OnLose(args);
+                }
+
+                tries++;
             }
 
             void OnNumberGuessed(CustomEventArgs e)
             {
+                e.Message = "You WIN";
+                e.Tries = tries;
+                e.Num = userNum;
 
+                GameFinished?.Invoke(this, e);                         
+              
+            }
+            void OnLose(CustomEventArgs e)
+            {
+                e.Message = "You lose";
+                e.Tries = tries;
+                e.Num = numToGuess;
+                GameFinished?.Invoke(this, e);
             }
         }
     }
 
     class Program
     {
+        static void HandleCustomEvent(object sender, CustomEventArgs e)
+        {
+            Console.WriteLine("Game result:");
+            Console.WriteLine(e.Message);
+            Console.WriteLine($"Num in Mind {e.Num}");
+            Console.WriteLine($"Tries: {e.Tries}");
+        }
         static void Main(string[] args)
         {
+            GuessNum guess = new GuessNum();
+            guess.GameFinished += HandleCustomEvent;
+
+            guess.Start();
+
+            Console.ReadKey();
         }
     }
 }
