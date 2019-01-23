99d6e198187f943e7915eb3f313aedb8fdb3b7b7
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-23 12:26:16 +0200

    guessing num

---
+++ EssentialLessons/Fiddle/Events_Fiddle/EventsFiddle_8/EventsFiddle_8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFiddle_8
+{
+    class CustomEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+        public int Tries { get; set; }
+        public int Num { get; set; }
+    }
+
+    class GuessNum
+    {
+        public event EventHandler<CustomEventArgs> GameFinished;
+
+        public void Start()
+        {           
+            int numToGuess = (new Random()).Next(1,10);
+            int userNum;
+
+            Console.WriteLine("You have 3 tries to guess number from 1 to 10");
+
+            for (int i=0; i<3; i++)
+            {
+                Console.WriteLine($"Try #{i} Enter num:");
+                userNum = int.Parse(Console.ReadLine());
+
+                if (numToGuess == userNum)
+                {
+
+                }
+            }
+
+            void OnNumberGuessed(CustomEventArgs e)
+            {
+
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
