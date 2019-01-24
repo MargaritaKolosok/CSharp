2401bc261810df77605b2b2b44f4dcb71f125158
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-24 16:34:43 +0200

    events

---
+++ EssentialLessons/Fiddle/Events_Fiddle/EventsFiddle10/EventsFiddle10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsFiddle10
+{
+    public interface IDrawingObject
+    {
+        event EventHandler OnDrawn;
+    }
+    public interface IShape
+    {
+        event EventHandler OnDrawn;
+    }
+
+    public class Shape : IDrawingObject, IShape
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
