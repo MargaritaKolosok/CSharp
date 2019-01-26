3b54bc9f78e4873a96b9448aa4a4c65b962f106b
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-26 10:42:41 +0200

    generics

---
+++ EssentialLessons/Lesson_10/Task2/Task2/IMyList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    interface IMyList<T>
+    {
+        void Add(T element);
+        T this[int index] { get; }
+        int Count { get; }
+        void Clear();
+        bool Contains(T element);
+    }
+}
