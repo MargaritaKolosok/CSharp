132379b5a1b2f4b650e6e69464e5f6624c7cec9c
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-26 20:06:28 +0200

    generics

---
+++ EssentialLessons/Lesson_10/Task3/Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Используя Visual Studio, создайте проект по шаблону Console Application.  
+Создайте класс MyDictionary<TKey,TValue>.
+Реализуйте в простейшем приближении возможность
+использования его экземпляра аналогично экземпляру класса Dictionary (Урок 6 пример 5).
+Минимально требуемый интерфейс взаимодействия с экземпляром,
+должен включать метод добавления пар элементов,
+индексатор для получения значения элемента по указанному индексу
+и свойство только для чтения для получения общего количества пар элементов.  
+ 
+ * */
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("table", "стол");
+            myDictionary.Add("cow", "корова");
+            myDictionary.Add("dog", "собака");
+            myDictionary.Add("cat", "кот");
+            myDictionary.Add("hourse", "лошадь");
+
+            Console.WriteLine(myDictionary.Count);
+
+            Console.WriteLine(myDictionary["cow"]);
+            Console.WriteLine(myDictionary["cowsss"]);
+
+            Console.ReadKey();
+           
+
+        }
+    }
+}
