9075eeb4de21cf80b2c8c7a2eddd9946cabeebb3
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-27 09:24:00 +0200

    generics

---
+++ EssentialLessons/Lesson_10/Task4/Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Создайте расширяющий метод: public static T[] GetArray<T>(this MyList<T> list) 
+
+Примените расширяющий метод к экземпляру типа MyList<T>,
+разработанному в домашнем задании 2 для данного урока.
+Выведите на экран значения элементов массива, который вернул расширяющий метод GetArray(). 
+
+ *
+ *
+ */
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
