89566ba5772ff9a51adc99ede485d15c3526a6ce
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-28 10:48:33 +0200

    generics

---
+++ EssentialLessons/Lesson_11/Task_2/Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Создайте класс CarCollection<T>. Реализуйте в простейшем приближении возможность
+использования его экземпляра для создания парка машин. Минимально требуемый интерфейс
+взаимодействия с экземпляром, должен включать метод добавления машин с названием машины и
+года ее выпуска, индексатор для получения значения элемента по указанному индексу и свойство
+только для чтения для получения общего количества элементов.
+Создайте метод удаления всех машин автопарка. 
+ */
+namespace Task_2
+{
+    interface Car
+    {        
+        void Add(string Name, int Year);
+        Car this[int index] { get; }
+        int Count { get; }        
+    }
+
+    class CarCollection<T> where T : Car
+    {
+        List<T> CarList = new List<T>();
+        public void Add(string Name, int Year)
+        {
+           
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
