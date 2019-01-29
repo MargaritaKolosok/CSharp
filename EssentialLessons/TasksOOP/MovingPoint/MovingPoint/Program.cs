8d72f1cb13193b33ac639483436bac79979826f1
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-01-29 21:31:21 +0200

    moveKey

---
+++ EssentialLessons/TasksOOP/MovingPoint/MovingPoint/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingPoint
+{
+    class Point
+    {
+        public delegate void MyHandler();
+
+        public event MyHandler MoveLeft;
+        public event MyHandler MoveRight;
+        public event MyHandler MoveUp;
+        public event MyHandler MoveDown;
+
+        public void OnLeft()
+        {
+            MoveLeft?.Invoke();
+        }
+        public void OnRight()
+        {
+            MoveRight?.Invoke();
+        }
+        public void OnUp()
+        {
+            MoveUp?.Invoke();
+        }
+        public void OnDown()
+        {
+            MoveDown?.Invoke();
+        }
+
+        public void StartMove()
+        {
+            ConsoleKeyInfo keyPressed;
+           
+            keyPressed = Console.ReadKey();
+
+            Console.WriteLine("Key pressed" + keyPressed);           
+        }
+    }
+    class Program
+    {
+        static void LeftHandler()
+        {
+            Console.WriteLine("Left");
+        }
+        static void RightHandler()
+        {
+            Console.WriteLine("Right");
+        }
+        static void UpHandler()
+        {
+            Console.WriteLine("Up");
+        }
+        static void DownHandler()
+        {
+            Console.WriteLine("Down");
+        }
+
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.MoveLeft += LeftHandler;
+            point.MoveRight += RightHandler;
+            point.MoveUp += UpHandler;
+            point.MoveDown += DownHandler;
+
+            point.OnDown();
+            point.OnUp();
+            point.OnRight();
+            point.OnLeft();
+
+            point.StartMove();
+
+            Console.ReadKey();
+        }
+       
+    }
+}
