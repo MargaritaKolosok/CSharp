17c68b0fabede42f022a4dcc9c980809f84f9092
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-02-07 22:43:43 +0200

    Update Program.cs

--- EssentialLessons/Fiddle/Move/Move/Program.cs
+++ EssentialLessons/Fiddle/Move/Move/Program.cs
@@ -6,17 +6,44 @@ using System.Threading.Tasks;
 
 namespace Move
 {
+    class Point
+    {
+        public int Left { get; set; }
+        public int Top { get; set; }
+    }
+    class Wall
+    {
+        public int Left { get; set; }
+        public int Top { get; set; }
+    }
     /*
       class который хранит  состояние поля: стенки, позицию курсора.  */
     class Field
     {
+        int barricades;
+        public Wall[] WallsArray;
 
+        public Field(int barricades) => this.barricades = barricades;
+
+        public Point point;
     }
     /*
       класс, который отвечает за отрисовку поля на консоль. */
     class FieldOnConsole
     {
+        public int Barricades { get; set; }
+        public void CreateWalls()
+        {
+            Field field = new Field(Barricades)
+            {
+                WallsArray = new Wall[Barricades]
+            };
+        }
+        public void SetStartCursor()
+        {
 
+        }
+            
     }
     /*
     класс который отвечает за перемещение курсора по полю.  */
