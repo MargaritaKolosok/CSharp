8e40a3ef0b21341d95588059ec2e83cea91809bb
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-02-07 22:25:59 +0200

    movingDot

---
+++ EssentialLessons/Fiddle/Move/Move/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Move
+{
+    /*
+      class который хранит  состояние поля: стенки, позицию курсора.  */
+    class Field
+    {
+
+    }
+    /*
+      класс, который отвечает за отрисовку поля на консоль. */
+    class FieldOnConsole
+    {
+
+    }
+    /*
+    класс который отвечает за перемещение курсора по полю.  */
+    class MoveOnField
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
