d0dca2d857c76201b2881fb1c1e35cf265be4977
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-02-08 22:52:52 +0200

    Update Program.cs

--- EssentialLessons/Fiddle/Move/Move/Program.cs
+++ EssentialLessons/Fiddle/Move/Move/Program.cs
@@ -25,28 +25,91 @@ namespace Move
     }
     class Map
     {
-        int[] Border;
-        int[] Walls;
-        int width = 80, height = 44;
+        int width = 20, height = 20;
+        char[,] Walls = new char[20, 20];
+
         public Map()
-        {                       
-            Console.SetWindowSize(width, height);
-            Console.CursorVisible = false;
-            DrawBorder();
-        }
-        void DrawBorder()
         {
+            Console.CursorVisible = false;
+            GenerateBorder();
+            DrawBorder();           
+        }
+        void Barricade(char point, int left, int top)
+        {
+            Console.SetCursorPosition(left, top);
+            Console.Write(point);
+        }
+        void GenerateBorder()
+        { 
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-                    if (i == 0 || i == width - 1) Console.Write("*");
-                    else if (j == 0 || j == height - 1) Console.Write("*");
-                    else Console.Write(" ");
+                    if (i == 0 || i == width - 1)
+                    {
+                        Walls[i, j] = '*';                      
+                    }
+                    else if (j == 0 || j == height - 1)
+                    {
+                        Walls[i, j] = '*';                      
+                    }
+                    else
+                    {
+                        Walls[i, j] = ' ';                      
+                    }
+                }
+            }
+            GenerateBarricades();
+        }
+        void GenerateBarricades()
+        {
+            Random random = new Random();
+            int counter = 0;
+            int x, y;
+            void Random()
+            {
+                x = random.Next(0, width);
+                y = random.Next(0, width);
+            }
+            while(counter < 10)
+            {
+                Random();
+
+                if (Walls[x, y] != '*' && Walls[x, y] != 'X')
+                {
+                    Walls[x, y] = 'X';
+                    counter++;
+                }             
+                
+            }
+           
+            
+        }
+        void DrawBorder()
+        {
+            for (int i=0; i< width; i++)
+            {
+                for (int j=0; j<height; j++)
+                {
+                    Console.Write(Walls[i,j]);
                 }
                 Console.WriteLine();
             }
         }
+/*    void SetWalls()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                int x = random.Next(0, 43);
+                int y = random.Next(0, 43);
+                Walls[x, y] = 'X';
+            }
+        }
+
+  */
+
+
         void Clear()
         {
             Console.Clear();
