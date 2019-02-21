e2b3dd5e3854c424f01f30ed7888a95ddf1815b8
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-02-21 12:04:43 +0200

    thread

---
+++ EssentialLessons/TasksOOP/ThreadFiddle/ThreadTask_1/ThreadTask_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadTask_1
+{
+    class Program
+    {
+        public static void Count()
+        {
+            for (int i=1; i<9; i++)
+            {
+                Console.WriteLine("Secondary thread");
+                Console.WriteLine(i*i);
+                Thread.Sleep(300);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(new ThreadStart(Count));
+            myThread.Start();
+
+            for (int i=1; i<9; i++)
+            {
+                Console.WriteLine("Main Thread");
+                Thread.Sleep(400);
+            }
+            Console.ReadLine();
+        }
+    }
+}
