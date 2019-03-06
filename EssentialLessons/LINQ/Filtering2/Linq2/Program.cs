20713f6ae697e6f7caaebd063d3bf95d59447384
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-06 21:54:25 +0200

    linq

--- EssentialLessons/LINQ/Filtering2/Linq2/Program.cs
+++ EssentialLessons/LINQ/Filtering2/Linq2/Program.cs
@@ -9,11 +9,29 @@ namespace Linq2
     class User
     {
         public string Name { get; set; }
+        public int Age { get; set; }
     }
+
     class Program
     {
         static void Main(string[] args)
         {
+            List<User> users = new List<User>();
+
+            users.Add(new User { Name = "Sam", Age = 43 });
+            users.Add(new User { Name = "Tom", Age = 33 });
+
+            var items = from u in users
+                        select new
+                        {
+                            FirstName = u.Name,
+                            DateOfBirth = DateTime.Now.Year - u.Age
+                        };
+            foreach (var n in items)
+            {
+                Console.WriteLine("{0} - {1}", n.FirstName, n.DateOfBirth);
+            }
+            Console.ReadKey();
         }
     }
 }
