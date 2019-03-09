90bb2deae1b05a5150f87c867aa20d9cdda6f492
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-09 19:31:55 +0200

    except

---
+++ EssentialLessons/LINQ/Filtering2/Mnojestva/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mnojestva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] soft = { "Microsoft", "Google", "Apple" };
+            string[] hard = { "Apple", "IBM", "Samsung" };
+
+            var result = soft.Except(hard);
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
