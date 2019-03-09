66c20f862b17a3c9e66b5b60fda4f2c634712f46
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-09 19:37:58 +0200

    Update Program.cs
    concat,
    distinct

--- EssentialLessons/LINQ/Filtering2/Mnojestva/Program.cs
+++ EssentialLessons/LINQ/Filtering2/Mnojestva/Program.cs
@@ -15,7 +15,45 @@ namespace Mnojestva
 
             var result = soft.Except(hard);
 
+            var result2 = soft.Intersect(hard);
+
+            var resultUnion = soft.Union(hard);
+            Console.WriteLine();
+            Console.WriteLine("Except");
+
             foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Intersect");
+
+            foreach (string s in result2)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Union");
+
+            foreach (string s in resultUnion)
+            {
+                Console.WriteLine(s);
+            }
+
+            var resultConcat = soft.Concat(hard);
+            Console.WriteLine();
+            Console.WriteLine("Concat:");
+
+            foreach (string s in resultConcat)
+            {
+                Console.WriteLine(s);
+            }
+
+            var resultDistinct = resultConcat.Distinct();
+            Console.WriteLine();
+            Console.WriteLine("Distinct:");
+
+            foreach (string s in resultDistinct)
             {
                 Console.WriteLine(s);
             }
