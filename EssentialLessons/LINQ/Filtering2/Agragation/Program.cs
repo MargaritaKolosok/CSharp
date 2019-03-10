cd978ed65f0c54122347eb56ffacd5b086cfbfc9
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-10 22:51:38 +0200

    count linq

--- EssentialLessons/LINQ/Filtering2/Agragation/Program.cs
+++ EssentialLessons/LINQ/Filtering2/Agragation/Program.cs
@@ -10,6 +10,14 @@ namespace Agragation
     {
         static void Main(string[] args)
         {
+            int[] numbers = { 1, 2, 3, 4, 10, 34, 55, 66, 77, 88 };
+
+            int size = numbers.Count(i => i%2 ==0 && i > 10);
+            int sum = numbers.Sum();
+
+            Console.WriteLine(size);
+            Console.WriteLine(sum);
+            Console.ReadKey();
         }
     }
 }
