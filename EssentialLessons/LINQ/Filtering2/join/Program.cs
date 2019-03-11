1dd0a823820435f9a611b64f63617cfee685dc96
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-11 22:14:54 +0200

    join

---
+++ EssentialLessons/LINQ/Filtering2/join/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace join
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public string Team { get; set; }
+    }
+    class Team
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Team> teams = new List<Team>()
+            {
+                new Team { Name = "Бавария", Country ="Германия" },
+                new Team { Name = "Барселона", Country ="Испания" }
+            };
+            List<Player> players = new List<Player>()
+            {
+                new Player {Name="Месси", Team="Барселона"},
+                new Player {Name="Неймар", Team="Барселона"},
+                new Player {Name="Роббен", Team="Бавария"}
+            };
+
+            var result = players.Join(teams,
+                p => p.Team,
+                t => t.Name,
+                (p,t) => new { Name = p.Name, Team = p.Team, Country = t.Country}
+                );
+
+            foreach (var player in result )
+            {
+                Console.WriteLine("{0}, {1}, {2}",player.Name, player.Team, player.Country);
+            }
+            Console.ReadKey();
+        }
+    }
+}
