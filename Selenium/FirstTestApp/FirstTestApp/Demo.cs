f2afd1c1462ebe5cbb3b19572551a4b63c0ea31d
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-20 21:25:12 +0200

    Selenium

--- Selenium/FirstTestApp/FirstTestApp/Demo.cs
+++ Selenium/FirstTestApp/FirstTestApp/Demo.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
 
 namespace FirstTestApp
 {
     class Demo
     {
+        IWebDriver driver;
+
+        [SetUp]
+
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver("C:\\Charp_git\\CSharp\\Selenium\\FirstTestApp\\packages\\Selenium.WebDriver.ChromeDriver.73.0.3683.68\\driver\\win32");
+        }
+            [Test]
+
+        public void Test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
     }
 }
--- Selenium/FirstTestApp/FirstTestApp/Program.cs
+++ Selenium/FirstTestApp/FirstTestApp/Program.cs
@@ -10,6 +10,7 @@ namespace FirstTestApp
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
