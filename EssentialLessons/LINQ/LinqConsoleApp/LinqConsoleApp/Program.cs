84e4310c68fbc21316ebcd1cd61244dae40408d0
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-26 20:01:01 +0200

    linq fiddle

---
+++ EssentialLessons/LINQ/LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
