8440c884a190e0825569ce1c242628b8e9212831
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-28 11:49:20 +0200

    first selenium sample test

--- Selenium/SeleniumTutorialY1/SeleniumTutorialY1/Program.cs
+++ Selenium/SeleniumTutorialY1/SeleniumTutorialY1/Program.cs
@@ -5,22 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
 
 namespace SeleniumTutorialY1
 {
     class Program
     {
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
+        {            
+        }
+
+        [SetUp]
+        public void Initialize()
         {
-            IWebDriver driver = new ChromeDriver();
-
             driver.Navigate().GoToUrl("https://www.google.com");
+        }
 
+        [Test]
+        public void ExecuteTest()
+        {         
             IWebElement element = driver.FindElement(By.Name("q"));
             element.SendKeys("Selenium");
-
+        }
+        [TearDown]
+        public void CleanUp()
+        {
             driver.Close();
-
         }
     }
 }
