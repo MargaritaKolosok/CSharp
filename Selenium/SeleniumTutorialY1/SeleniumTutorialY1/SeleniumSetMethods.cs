5173a87d379b6cdd845bc54fb237fb1d9b85310c
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-29 09:01:14 +0200

    selenium

---
+++ Selenium/SeleniumTutorialY1/SeleniumTutorialY1/SeleniumSetMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTutorialY1
+{
+    class SeleniumSetMethods
+    {
+        public static void EnterText(IWebElement driver, string element, string value, string elementType)
+        {          
+            if (elementType == "Id")
+            {
+                driver.FindElement(By.Id(element)).SendKeys(value); ;
+            }
+            else if (elementType == "Name")
+            {
+                driver.FindElement(By.Name(element)).SendKeys(value); ;
+            }           
+        }
+
+        public static void Click(IWebElement driver, string element, string elementType)
+        {
+            if (elementType == "Id")
+            {
+                driver.FindElement(By.Id(element)).Click();
+            }
+            else if (elementType == "Name")
+            {
+                driver.FindElement(By.Name(element)).Click(); 
+            }
+        }
+        public static void SelectDropDown(IWebElement driver, string element, string value, string elementType)
+        {
+
+        }
+    }
+
+}
