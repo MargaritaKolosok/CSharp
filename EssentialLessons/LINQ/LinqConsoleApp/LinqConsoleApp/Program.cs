e647e16370381965ac3f66f054a5ad9f55c90d1f
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-03-29 22:20:26 +0200

    linq

--- EssentialLessons/LINQ/LinqConsoleApp/LinqConsoleApp/Program.cs
+++ EssentialLessons/LINQ/LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -8,6 +8,40 @@ using System.Data.Linq.Mapping;
 
 namespace LinqConsoleApp
 {
+    [Table(Name = "Customers")]
+    public class Customer
+    {
+        private string _CustomerID;
+
+        [Column(IsPrimaryKey = true, Storage = "_CustomerID")]
+
+        public string CustomerID
+        {
+            get
+            {
+                return this._CustomerID;
+            }
+            set
+            {
+                this._CustomerID = value;
+            }
+        }
+
+        private string _City;
+        [Column(Storage = "_City")]
+        public string City
+        {
+            get
+            {
+                return this._City;
+            }
+            set
+            {
+                this._City = value;
+            }
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
