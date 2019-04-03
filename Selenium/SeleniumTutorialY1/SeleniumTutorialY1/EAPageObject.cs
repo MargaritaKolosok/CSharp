d659a51842de56317bc8d8713d4628841d558e20
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-04-03 22:49:21 +0300

    selenium

---
+++ Selenium/SeleniumTutorialY1/SeleniumTutorialY1/EAPageObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace SeleniumTutorialY1
+{
+    class EAPageObject
+    {
+        [FindsBy(How = How.Id, Using = "TitleId")]
+        public IWebElement TitleId { get; set; }
+
+        [FindsBy(How = How.Name, Using = "Initial")]
+        public IWebElement txtInitial { get; set; }
+    }
+}
