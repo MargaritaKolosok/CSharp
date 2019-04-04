6138ca885e7ccfaafdb55c499b311abb0ef587a8
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-04-04 20:31:01 +0300

    api models

---
+++ EssentialLessons/Fiddle/ImageGrid/ImageGrid/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageGrid.Models
+{
+    public class TodoContext : DbContext
+    {
+
+    }
+}
