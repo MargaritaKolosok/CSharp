8248521f6599543494ddb19d29e3b46e72a610e6
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-04-05 21:27:31 +0300

    Create Startup.cs

---
+++ EssentialLessons/Fiddle/ImageGrid/ImageGrid/Models/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using ImageGrid.Models;
+
+namespace ImageGrid.Models
+{
+    public class Startup
+    {
+
+    }
+}
