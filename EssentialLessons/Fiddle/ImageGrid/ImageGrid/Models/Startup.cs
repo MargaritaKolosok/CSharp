90ece718037fd509d6962cad8e7898b0a53426d1
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-04-10 10:13:45 +0300

    Update Startup.cs

--- EssentialLessons/Fiddle/ImageGrid/ImageGrid/Models/Startup.cs
+++ EssentialLessons/Fiddle/ImageGrid/ImageGrid/Models/Startup.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Microsoft.EntityFrameworkCore;
-using ImageGrid.Models;
 
 namespace ImageGrid.Models
 {
     public class Startup
     {
-
     }
 }
