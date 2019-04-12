bea614fc31609d295608ec51820f773b7123c5fe
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-04-12 11:10:21 +0300

    todoapi

---
+++ EssentialLessons/MSDN Projects/TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        // Определяет класс контроллера API без методов.
+
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+            if (_context.TodoItems.Count() ==0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+
+            }
+        }
+    }
+}
