ae06ef1418d66a6b647e6bf7df32bf6df70c0464
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-04-23 09:07:53 +0300

    RazorPages

---
+++ EssentialLessons/MSDN Projects/RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        // Поле ID является обязательным для первичного ключа базы данных.
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
