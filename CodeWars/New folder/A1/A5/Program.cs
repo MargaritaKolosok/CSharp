6b5f958ea8323a39969dc304a2bd1573eca228b1
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-04-26 10:22:44 +0300

    codewars

---
+++ CodeWars/New folder/A1/A5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5
+{
+    class Program
+    {
+        public static string DoneOrNot(int[][] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+               
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[])
+                    {
+
+                    }
+                }
+            }
+
+            bool CheckUniq(int num)
+                {
+                    string temp = board[]
+                }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
