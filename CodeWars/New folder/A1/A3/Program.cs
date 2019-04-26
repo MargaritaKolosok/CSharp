c31b6f7d486a78629b7b7b5801b0b067d49bddee
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-04-26 08:45:29 +0300

    codewars

---
+++ CodeWars/New folder/A1/A3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace A3
+{
+    //Write simple.camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings.
+    //All words must have their first letter capitalized without spaces.
+
+    static class Extention
+    {
+        public static string CamelCase(this string str)
+        {
+            TextInfo CultInfo = new CultureInfo("en-US", false).TextInfo;
+            str = CultInfo.ToTitleCase(str);
+            str = str.Replace(" ", "");
+            return str;
+        }
+    }
+
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
