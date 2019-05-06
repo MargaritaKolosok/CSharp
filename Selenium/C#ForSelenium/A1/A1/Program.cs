dfebffba3775f5aebfc3e22b5f091126f6bb3698
MargaritaLadybird <31467168+MargaritaLadybird@users.noreply.github.com>
2019-05-06 22:09:51 +0300

    generic collections for c#

---
+++ Selenium/C#ForSelenium/A1/A1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A1
+{   
+    class Program
+    {
+        public static void GenericCollection()
+        {
+            string[] user1 = new string[] { "MyName", "MyCountry", "Age" };
+            string[] user2 = new string[] { "MyName", "MyCountry", "Age" };
+            string[] user3 = new string[] { "MyName", "MyCountry", "Age" };
+
+            Dictionary<int, string[]> dict = new Dictionary<int, string[]>();
+            dict.Add(1, user1);
+            dict.Add(2, user2);
+            dict.Add(3, user3);
+
+            foreach (var value in dict)
+            {
+                string[] usersInfo = value.Value;
+
+                foreach (var user in usersInfo)
+                {
+                    Console.WriteLine(user);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GenericCollection();
+            Console.ReadKey();
+        }
+    }
+}
