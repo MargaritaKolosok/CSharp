2eb9eb92cff87acf7f8e19d43a1e4f96049c5cf1
MargaritaKolosok <31467168+MargaritaLadybird@users.noreply.github.com>
2019-06-19 14:02:56 +0300

    Selenium, nunit

--- Selenium/SeleniumFirst/SeleniumFirst/Program.cs
+++ Selenium/SeleniumFirst/SeleniumFirst/Program.cs
@@ -5,26 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using NUnit.Framework;
+
 
 namespace SeleniumFirst
 {
     class Program
     {
+        // Create reference for our driver
+        IWebDriver driver = new ChromeDriver();
+
         static void Main(string[] args)
-        {
-            // Create reference for our driver
-
-            IWebDriver driver = new ChromeDriver();
-
-            // Navigate to the google page
-            driver.Navigate().GoToUrl("http://google.com");
-
-            IWebElement element = driver.FindElement(By.Name("q"));
-
-            element.SendKeys("Ameria AG");
-
-            element.SendKeys(Keys.Enter);
+        {           
 
         }
+
+        [SetUp]
+        public void Initialize()
+        {
+            // Navigate to the google page
+            driver.Navigate().GoToUrl("http://google.com");
+            Console.WriteLine("Opened URL");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("Ameria AG");
+            element.SendKeys(Keys.Enter);
+
+            Console.WriteLine("Executed test");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+
+            Console.WriteLine("Browser closed");
+        }
+
     }
 }
