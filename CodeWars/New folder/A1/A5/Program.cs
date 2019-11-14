be562217a89a3f91d1f2492944a23644650f038f
MargaritaKolosok <31467168+MargaritaKolosok@users.noreply.github.com>
2019-11-14 11:36:39 +0200

    codewars

--- CodeWars/New folder/A1/A5/Program.cs
+++ CodeWars/New folder/A1/A5/Program.cs
@@ -12,10 +12,26 @@ namespace A5
         {
             return input.ToLower().Count(i => i == 'x') == input.ToLower().Count(i => i == 'o');
         }
+        public static int Divisors(int n)
+        {
+            return Enumerable
+                .Range(1, n)
+                .Select(x => x)
+                .Count(x => n % x == 0);
+        }
+
+        public static string PrinterError(String s)
+        {
+            s.ToLower();
+            return s.Count(x => x > 'm') + "/" + s.Count();
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine(XO("xxxooXXOo"));
+            Console.WriteLine(Divisors(100));
+            // 1,2,4,5,10,20,25,50,100
+            Console.WriteLine(PrinterError("aaaxbbbbyyhwawiwjjjwwm"));
             Console.ReadLine();
         }
     }
