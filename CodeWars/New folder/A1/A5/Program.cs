05127354908d1b29fce47e5da0f46e653e2c17fa
MargaritaKolosok <31467168+MargaritaKolosok@users.noreply.github.com>
2019-11-15 11:14:27 +0200

    CodeWars

--- CodeWars/New folder/A1/A5/Program.cs
+++ CodeWars/New folder/A1/A5/Program.cs
@@ -26,12 +26,32 @@ namespace A5
             return s.Count(x => x > 'm') + "/" + s.Count();
         }
 
+        public static int FindEvenIndex(int[] arr)
+        {
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (arr.Take(i).Sum() == arr.Skip(i + 1).Sum()) { return i; }
+            }
+            return -1;
+        }
+
+        public static string OddOrEven(int[] array)
+        {
+            return (array.Sum() % 2 == 0) ? "even" : "odd";            
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine(XO("xxxooXXOo"));
+
             Console.WriteLine(Divisors(100));
-            // 1,2,4,5,10,20,25,50,100
+             // 1,2,4,5,10,20,25,50,100
+
             Console.WriteLine(PrinterError("aaaxbbbbyyhwawiwjjjwwm"));
+
+            int[] oddOrEvenArray = { 1, 2, 3, 4, 5, 6, 7, 8, 99, 3,4,4,5,5,5 };
+            Console.WriteLine(OddOrEven(oddOrEvenArray));
+
             Console.ReadLine();
         }
     }
