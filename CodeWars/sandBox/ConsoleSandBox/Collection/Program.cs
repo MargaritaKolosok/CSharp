c7ff71a5fb2b3d9a36efd36abccc0a6890bf3941
MargaritaKolosok <31467168+MargaritaKolosok@users.noreply.github.com>
2020-04-17 11:07:34 +0300

    Collection

---
+++ CodeWars/sandBox/ConsoleSandBox/Collection/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+
+namespace Collection
+{
+    class Collection : IEnumerable
+    {
+        int[] array = new int[5] { 1, 2, 3, 45, 5 };
+        public IEnumerator GetEnumerator()
+        {
+            return new Iterator(this);
+        }
+
+        class Iterator : IEnumerator
+        {
+
+            int[] array;
+            int currentPosition = -1;
+
+            public Iterator(Collection col)
+            {
+                array = col.array;
+            }            
+
+            public object Current
+            {
+                get
+                {
+                    return array[currentPosition];
+                }                 
+            }
+            public bool MoveNext()
+            {
+                if(currentPosition < array.Length - 1)
+                {
+                    currentPosition++;
+                    return true;
+                }
+                return false;
+            }
+
+            public void Reset()
+            {
+                currentPosition = -1;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection col = new Collection();
+            foreach(var item in col)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(col.GetEnumerator());           
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
